e777a73815c5cdb1ef4a9c05f113aeb2b559f4cd
Marc Duby <marc_duby@post.harvard.edu>
2020-09-14 16:34:33 -0400

    Magma: working last step

---
+++ magma/src/main/scala/MagmaCombineNcbiGenePValuesStage.scala
@@ -0,0 +1,38 @@
+package org.broadinstitute.dig.aggregator.methods.magma
+
+import org.broadinstitute.dig.aggregator.core._
+import org.broadinstitute.dig.aws._
+import org.broadinstitute.dig.aws.emr._
+import org.broadinstitute.dig.aws.Ec2.Strategy
+
+/** After meta-analysis, this stage finds the most significant variant
+  * every 50 kb across the entire genome.
+  */
+class MagmaCombineNcbiGenePValuesStage(implicit context: Context) extends Stage {
+  import MemorySize.Implicits._
+
+  val magmaGenePValues: Input.Source = Input.Source.Success("out/magma/step4GenePValues/*/")
+
+  /** The output of gene pvalue analysis is the input for top results file. */
+  override val sources: Seq[Input.Source] = Seq(magmaGenePValues)
+
+  /** Process top associations for each phenotype. */
+  override val rules: PartialFunction[Input, Outputs] = {
+    case magmaGenePValues(phenotype) => Outputs.Named(phenotype)
+  }
+
+  /** Simple cluster with more memory. */
+  override val cluster: ClusterDef = super.cluster.copy(
+//    masterInstanceType = Strategy.generalPurpose(mem = 64.gb),
+//    slaveInstanceType = Strategy.generalPurpose(mem = 32.gb),
+    instances = 4
+  )
+
+  /** Build the job. */
+  override def make(output: String): Job = {
+    val script    = resourceUri("combineNcbiGenePValues.py")
+    val phenotype = output
+
+    new Job(Job.PySpark(script, phenotype))
+  }
+}
