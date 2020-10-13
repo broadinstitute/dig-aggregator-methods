29e973cf52bb5f2e0149a75290d471418cfb216c
Jeffrey Massung <massung@gmail.com>
2020-10-13 12:48:55 -0500

    Removed isParallel

--- gregor/src/main/scala/MergeRegionsStage.scala
+++ gregor/src/main/scala/MergeRegionsStage.scala
@@ -49,7 +49,7 @@ class MergeRegionsStage(implicit context: Context) extends Stage {
     // run in parallel across jobs and clusters. So, this helps improve the
     // overall performance.
 
-    new Job(steps, isParallel = true)
+    new Job(steps)
   }
 
   /** Before the jobs actually run, perform this operation.
