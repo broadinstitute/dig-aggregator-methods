79bb6c406be46476777e16c1e2a47f0a38783ab2
Jeffrey Massung <massung@gmail.com>
2021-03-23 08:43:32 -0500

    MAF from VEP.

--- vep/src/main/resources/common.py
+++ vep/src/main/resources/common.py
@@ -4,6 +4,7 @@ import os.path
 import platform
 
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, explode
 
 # where in S3 VEP data (input and output) is
 S3DIR = 's3://dig-analysis-data/out/varianteffect'
@@ -23,15 +24,29 @@ def main():
     df = spark.read.json('%s/effects/*.json' % S3DIR)
     snp = spark.read.csv('%s/snp/*.csv' % S3DIR, sep='\t', header=True)
 
-    # use the most severe consequence to get genes and impact
+    # explode the consequences to get allele frequency
+    maf = df.select(df.id.alias('varId'), df.transcript_consequences) \
+        .withColumn('cqs', explode(df.transcript_consequences)) \
+        .select(
+            col('varId'),
+            col('cqs.gnomad_genomes_popmax_af').alias('maf'),
+        )
+
+    # keep the max frequency per variant
+    maf = maf.groupBy('varId').max('maf') \
+        .withColumnRenamed('max(maf)', 'maf')
+
+    # keep non-consequence field
     df = df.select(
         df.id.alias('varId'),
         df.most_severe_consequence.alias('consequence'),
         df.nearest,
     )
 
-    # join with the dbSNP
-    df = df.join(snp, 'varId', how='left_outer')
+    # join with frequency and dbSNP
+    df = df \
+        .join(maf, 'varId', how='left_outer') \
+        .join(snp, 'varId', how='left_outer')
 
     # output the common data in json format
     df.write.mode('overwrite').json('%s/common' % S3DIR)
