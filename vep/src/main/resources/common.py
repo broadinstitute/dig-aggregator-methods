4f096bc51443e870ac3ceb538ff22c5db9c2e2f0
Jeffrey Massung <massung@gmail.com>
2021-03-30 15:05:39 -0500

    AF and MAF for common VEP results.

--- vep/src/main/resources/common.py
+++ vep/src/main/resources/common.py
@@ -3,11 +3,46 @@
 import os.path
 import platform
 
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, explode
+from pyspark.sql import SparkSession, Row
 
 # where in S3 VEP data (input and output) is
 S3DIR = 's3://dig-analysis-data/out/varianteffect'
+
+
+def common_fields(row):
+    """
+    Extracts data from the colocated variant fields.
+    """
+    variant = next((v for v in row.colocated_variants or [] if v.allele_string == row.allele_string), None)
+
+    # no colocated variant found, just return the common data
+    if not variant:
+        return Row(
+            varId=row.id,
+            consequence=row.most_severe_consequence,
+            nearest=row.nearest,
+            dbSNP=None,
+            maf=None,
+            af=None,
+        )
+
+    # get the allele for frequency data
+    allele = row.id.split(':')[-1]
+
+    return Row(
+        varId=row.id,
+        consequence=row.most_severe_consequence,
+        nearest=row.nearest,
+        dbSNP=variant.id if variant.id.startswith('rs') else None,
+        maf=variant.minor_allele_freq,
+        af=variant.frequencies and Row(
+            eur=variant.frequencies[allele].eur,
+            amr=variant.frequencies[allele].amr,
+            afr=variant.frequencies[allele].afr,
+            eas=variant.frequencies[allele].eas,
+            sas=variant.frequencies[allele].sas,
+        ),
+    )
 
 
 def main():
@@ -20,22 +55,16 @@ def main():
     # create the spark context
     spark = SparkSession.builder.appName('vep').getOrCreate()
 
-    # load data
+    # load effect data
     df = spark.read.json('%s/effects/*.json' % S3DIR)
-    snp = spark.read.csv('%s/snp/*.csv' % S3DIR, sep='\t', header=True)
 
-    # keep non-consequence field
-    df = df.select(
-        df.id.alias('varId'),
-        df.most_severe_consequence.alias('consequence'),
-        df.nearest,
-    )
-
-    # join with dbSNP
-    df = df.join(snp, 'varId', how='left_outer')
-
-    # output the common data in json format
-    df.write.mode('overwrite').json('%s/common' % S3DIR)
+    # extract just the common fields and write them out
+    df.rdd \
+        .map(common_fields) \
+        .toDF() \
+        .write \
+        .mode('overwrite') \
+        .json('%s/common' % S3DIR)
 
     # done
     spark.stop()
