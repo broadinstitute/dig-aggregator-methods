796117f0abfb9c6f97024b6463f3abf72b96643d
Jeffrey Massung <massung@gmail.com>
2021-05-03 16:07:15 -0500

    Test failed, fixing.

--- bioindex/src/main/resources/cluster-bootstrap.sh
+++ bioindex/src/main/resources/cluster-bootstrap.sh
@@ -5,14 +5,14 @@ sudo yum install -y git
 # need to upgrade pip
 sudo python3 -m pip install --upgrade pip
 
-# upgrading pip moves it to a location root doesn't see
-#sudo ln -s /usr/local/bin/pip /bin/pip
-#sudo ln -s /usr/local/bin/pip3 /bin/pip3
+# upgrading pip moves it to a location root doesn't have in its PATH
+sudo ln -s /usr/local/bin/pip /bin/pip
+sudo ln -s /usr/local/bin/pip3 /bin/pip3
 
 # bioindex to get access to shared library functions
-sudo -E pip3 install git+git://github.com/broadinstitute/dig-bioindex.git@master#egg=bioindex
+sudo pip3 install git+git://github.com/broadinstitute/dig-bioindex.git@master#egg=bioindex
 
 # other python libs used by various stages
-sudo -E pip3 install matplotlib
-sudo -E pip3 install pandas
-sudo -E pip3 install statsmodels
+sudo pip3 install matplotlib
+sudo pip3 install pandas
+sudo pip3 install statsmodels
