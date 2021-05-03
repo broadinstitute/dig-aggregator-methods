1a919a5ad8096ff2bc6505e9226919000fe642d9
Jeffrey Massung <massung@gmail.com>
2021-05-03 15:43:07 -0500

    Fixed bootstrap.

--- bioindex/src/main/resources/cluster-bootstrap.sh
+++ bioindex/src/main/resources/cluster-bootstrap.sh
@@ -2,6 +2,13 @@
 
 sudo yum install -y git
 
+# need to upgrade pip
+sudo python3 -m pip install --upgrade pip
+
+# upgrading pip moves it to a location root doesn't see
+sudo ln -s /usr/local/bin/pip /bin/pip
+sudo ln -s /usr/local/bin/pip3 /bin/pip3
+
 # bioindex to get access to shared library functions
 sudo pip3 install git+git://github.com/broadinstitute/dig-bioindex.git@master#egg=bioindex
 
