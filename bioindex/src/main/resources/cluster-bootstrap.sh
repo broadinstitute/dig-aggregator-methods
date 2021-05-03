90b6f7ef459d5efde55e870a1657e0f7f6547940
Jeffrey Massung <massung@gmail.com>
2021-05-03 13:20:45 -0500

    Add git to bootstrap.

--- bioindex/src/main/resources/cluster-bootstrap.sh
+++ bioindex/src/main/resources/cluster-bootstrap.sh
@@ -1,7 +1,11 @@
 #!/bin/bash -xe
 
+sudo yum install -y git
+
+# bioindex to get access to shared library functions
 sudo pip3 install git+git://github.com/broadinstitute/dig-bioindex.git@master#egg=bioindex
 
+# other python libs used by various stages
 sudo pip3 install matplotlib
 sudo pip3 install pandas
 sudo pip3 install statsmodels
