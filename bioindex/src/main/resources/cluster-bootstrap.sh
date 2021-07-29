8c59150f7cf2472f9455e705c92e1694bd146bef
EC2 Default User <ec2-user@ip-10-0-0-84.ec2.internal>
2021-07-29 01:20:40 +0000

    In bioindex/src/main/resources/cluster-bootstrap.sh, fix versions of installed deps to make bootstrapping more deterministic; uninstall numpy so any system-wide numpy version won't conflict with the one Pandas pulls in

--- bioindex/src/main/resources/cluster-bootstrap.sh
+++ bioindex/src/main/resources/cluster-bootstrap.sh
@@ -13,6 +13,10 @@ sudo ln -s /usr/local/bin/pip3 /bin/pip3
 sudo pip3 install git+git://github.com/broadinstitute/dig-bioindex.git@master#egg=bioindex
 
 # other python libs used by various stages
-sudo pip3 install matplotlib
-sudo pip3 install pandas
-sudo pip3 install statsmodels
+#
+#Uninstall numpy so any system-wide numpy version won't conflict with the one Pandas pulls in
+sudo pip3 uninstall numpy
+#Fix versions to make this more deterministic
+sudo pip3 install matplotlib==3.4.2
+sudo pip3 install pandas==1.2.5
+sudo pip3 install statsmodels==0.12.2
