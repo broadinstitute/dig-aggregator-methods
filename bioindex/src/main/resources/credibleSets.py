5e7a632396cf1275360706136c6ae7026be4f278
psmadbec <psmadbec@broadinstitute.org>
2022-05-27 14:36:11 -0400

    tmp

--- bioindex/src/main/resources/credibleSets.py
+++ bioindex/src/main/resources/credibleSets.py
@@ -24,6 +24,7 @@ def main():
 
     # load all the credible sets for the phenotype
     df = spark.read.json(srcdir)
+    df = df.withColumn("chromosome", df["chromosome"].cast("string"))
 
     # sort by credible set and then locus
     df.coalesce(1) \
@@ -55,55 +56,55 @@ def main():
         .mode('overwrite') \
         .json(f'{outdir}/locus/{args.phenotype}')
 
-    # load joined regions
-    regions = spark.read.json('s3://dig-analysis-data/out/gregor/regions/joined/part-*')
-
-    # for each credible find regions that overlap the top variants
-    w = Window \
-        .partitionBy(['dataset', 'credibleSetId']) \
-        .orderBy(desc('posteriorProbability'))
-
-    # get the ranking of each variant in the credible set
-    df = df \
-        .withColumn('rank', rank().over(w)) \
-        .filter((col('rank') <= 20) | (col('posteriorProbability') > 0.002)) \
-        .select(
-            col('phenotype'),
-            col('dataset'),
-            col('credibleSetId'),
-            col('chromosome'),
-            col('position'),
-        )
-
-    # overlap condition
-    on = (df.chromosome == regions.chromosome) & \
-         (df.position >= regions.start) & \
-         (df.position < regions.end)
-
-    # for each credible set, find regions that overlap the top variants
-    df = df.join(regions.alias('regions'), on=on) \
-        .select(
-            col('phenotype'),
-            col('dataset'),
-            col('credibleSetId'),
-            col('regions.chromosome').alias('chromosome'),
-            col('regions.start').alias('start'),
-            col('regions.end').alias('end'),
-            col('regions.tissueId').alias('tissueId'),
-            col('regions.tissue').alias('tissue'),
-            col('regions.annotation').alias('annotation'),
-            col('regions.method').alias('method'),
-            col('regions.predictedTargetGene').alias('predictedTargetGene'),
-            col('regions.targetStart').alias('targetStart'),
-            col('regions.targetEnd').alias('targetEnd'),
-            col('regions.transcriptionStartSite').alias('transcriptionStartSite'),
-        )
-
-    # drop the variant position, sort by credible set id,
-    df.orderBy(['credibleSetId']) \
-        .write \
-        .mode('overwrite') \
-        .json(f'{outdir}/regions/{args.phenotype}')
+    # # load joined regions
+    # regions = spark.read.json('s3://dig-analysis-data/out/gregor/regions/joined/part-*')
+    #
+    # # for each credible find regions that overlap the top variants
+    # w = Window \
+    #     .partitionBy(['dataset', 'credibleSetId']) \
+    #     .orderBy(desc('posteriorProbability'))
+    #
+    # # get the ranking of each variant in the credible set
+    # df = df \
+    #     .withColumn('rank', rank().over(w)) \
+    #     .filter((col('rank') <= 20) | (col('posteriorProbability') > 0.002)) \
+    #     .select(
+    #         col('phenotype'),
+    #         col('dataset'),
+    #         col('credibleSetId'),
+    #         col('chromosome'),
+    #         col('position'),
+    #     )
+    #
+    # # overlap condition
+    # on = (df.chromosome == regions.chromosome) & \
+    #      (df.position >= regions.start) & \
+    #      (df.position < regions.end)
+    #
+    # # for each credible set, find regions that overlap the top variants
+    # df = df.join(regions.alias('regions'), on=on) \
+    #     .select(
+    #         col('phenotype'),
+    #         col('dataset'),
+    #         col('credibleSetId'),
+    #         col('regions.chromosome').alias('chromosome'),
+    #         col('regions.start').alias('start'),
+    #         col('regions.end').alias('end'),
+    #         col('regions.tissueId').alias('tissueId'),
+    #         col('regions.tissue').alias('tissue'),
+    #         col('regions.annotation').alias('annotation'),
+    #         col('regions.method').alias('method'),
+    #         col('regions.predictedTargetGene').alias('predictedTargetGene'),
+    #         col('regions.targetStart').alias('targetStart'),
+    #         col('regions.targetEnd').alias('targetEnd'),
+    #         col('regions.transcriptionStartSite').alias('transcriptionStartSite'),
+    #     )
+    #
+    # # drop the variant position, sort by credible set id,
+    # df.orderBy(['credibleSetId']) \
+    #     .write \
+    #     .mode('overwrite') \
+    #     .json(f'{outdir}/regions/{args.phenotype}')
 
     # done
     spark.stop()
