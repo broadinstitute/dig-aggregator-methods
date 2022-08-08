e3da8a5c304a121459aa1fd003fc834da579a9ba
Marc Duby <mduby@broadinstitute.org>
2022-08-07 22:24:10 -0400

    pathways: fixed issue that some files not always created

--- magma/src/main/resources/pathwayAssociations.sh
+++ magma/src/main/resources/pathwayAssociations.sh
@@ -17,18 +17,25 @@ ln -s "${MAGMA_DIR}/pathway-genes/pathwayGenes.txt" .
 aws s3 cp "${OUT_DIR}/staging/genes/${PHENOTYPE}/associations.genes.raw" .
 
 # run magma
+# NOTE: create seperate output directory since need to do recursive copy of output since some files don't always get created, so can't specify files to copy
 # "${MAGMA_DIR}/magma" --bfile ./g1000_eur --pval ./associations.csv ncol=subjects --gene-annot ./variants.genes.annot --out ./associations
-"${MAGMA_DIR}/magma" --gene-results associations.genes.raw --set-annot pathwayGenes.txt --out associations.pathways
+mkdir -p "output_${PHENOTYPE}"
+"${MAGMA_DIR}/magma" --gene-results associations.genes.raw --set-annot pathwayGenes.txt --out "output_${PHENOTYPE}/associations.pathways"
 
 # copy the output of magma back to S3
-aws s3 cp ./associations.pathways.gsa.out "${OUT_DIR}/staging/pathways/${PHENOTYPE}/associations.pathways.gsa.out"
-aws s3 cp ./associations.pathways.gsa.genes.out "${OUT_DIR}/staging/pathways/${PHENOTYPE}/associations.pathways.gsa.genes.out"
-aws s3 cp ./associations.pathways.gsa.sets.genes.out "${OUT_DIR}/staging/pathways/${PHENOTYPE}/associations.pathways.gsa.sets.genes.out"
-aws s3 cp ./associations.pathways.log "${OUT_DIR}/staging/pathways/${PHENOTYPE}/associations.pathways.log"
+cd "output_${PHENOTYPE}"
+aws s3 cp --recursive . "${OUT_DIR}/staging/pathways/${PHENOTYPE}/" 
+
+# aws s3 cp ./associations.pathways.gsa.out "${OUT_DIR}/staging/pathways/${PHENOTYPE}/associations.pathways.gsa.out"
+# aws s3 cp ./associations.pathways.gsa.genes.out "${OUT_DIR}/staging/pathways/${PHENOTYPE}/associations.pathways.gsa.genes.out"
+# aws s3 cp ./associations.pathways.gsa.sets.genes.out "${OUT_DIR}/staging/pathways/${PHENOTYPE}/associations.pathways.gsa.sets.genes.out"
+# aws s3 cp ./associations.pathways.log "${OUT_DIR}/staging/pathways/${PHENOTYPE}/associations.pathways.log"
 
 # aws s3 cp ./associations.genes.raw "${OUT_DIR}/staging/genes/${PHENOTYPE}/associations.genes.raw"
 # aws s3 cp ./associations.log "${OUT_DIR}/staging/genes/${PHENOTYPE}/associations.log"
 
 # delete the input and output files to save disk space for other steps
-rm associations.*
+# now cleanup output directory
+cd ..
+rm -rf "output_${PHENOTYPE}"
 # rm variants.genes.annot
