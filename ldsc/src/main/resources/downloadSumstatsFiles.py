10e36e57859556e52f34d55e62d24d6ed02068e0
psmadbec <psmadbec@broadinstitute.org>
2022-09-14 14:26:51 -0400

    [LDSC] Genetic Correlation Stage

---
+++ ldsc/src/main/resources/downloadSumstatsFiles.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import boto3
+import re
+import subprocess
+
+downloaded_files = '/mnt/var/ldsc'
+sumstat_files = f'{downloaded_files}/sumstats'
+
+
+def download_ancestry_sumstats():
+    s3 = boto3.resource('s3')
+    my_bucket = s3.Bucket('dig-analysis-data')
+    for file in my_bucket.objects.filter(Prefix='out/ldsc/sumstats/').all():
+        if re.fullmatch(f'.*\.sumstats\.gz$', file.key):
+            ancestry = re.findall(f'.*_(\w+).sumstats.gz', file.key)[0]
+            subprocess.check_call([
+                'aws', 's3', 'cp', f's3://dig-analysis-data/{file.key}', f'{sumstat_files}/{ancestry}/'
+            ])
+
+
+if __name__ == '__main__':
+    download_ancestry_sumstats()
