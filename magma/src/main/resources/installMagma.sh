2371c79179202d055fa7b306ec449fec6cd7fdc6
psmadbec <psmadbec@broadinstitute.org>
2022-09-21 18:15:27 -0400

    [Magma] Add ancestry specific and Mixed versions of variant associations

--- magma/src/main/resources/installMagma.sh
+++ magma/src/main/resources/installMagma.sh
@@ -15,7 +15,7 @@ aws s3 cp "${S3DIR}/bin/magma/g1000_eur.bim" .
 aws s3 cp "${S3DIR}/bin/magma/g1000_eur.fam" .
 aws s3 cp "${S3DIR}/bin/magma/g1000_eur.synonyms" .
 aws s3 cp "${S3DIR}/bin/magma/g1000_eur.synonyms" .
-aws s3 cp "${S3DIR}/out/magma/staging/pathway-genes/pathwayGenes.txt" "${MAGMA_DIR}"/pathway-genes/
+aws s3 cp "${S3DIR}/bin/magma/pathwayGenes.txt" "${MAGMA_DIR}"/pathway-genes/
 
 # copy and extract the magma program
 aws s3 cp "${S3DIR}/bin/magma/magma_v1.07bb_static.zip" .
--- magma/src/main/scala/VariantAssociationsStage.scala
+++ magma/src/main/scala/VariantAssociationsStage.scala
@@ -11,29 +11,60 @@ import org.broadinstitute.dig.aws.Ec2.Strategy
 class VariantAssociationsStage(implicit context: Context) extends Stage {
   import MemorySize.Implicits._
 
-  val bottomLine: Input.Source = Input.Source.Success("out/metaanalysis/trans-ethnic/*/")
-  val snps: Input.Source       = Input.Source.Success("out/varianteffect/snp/")
+  val ancestrySpecific: Input.Source = Input.Source.Success("out/metaanalysis/ancestry-specific/*/*/")
+  val mixedDatasets: Input.Source = Input.Source.Dataset("variants/*/*/*/")
+  val snps: Input.Source = Input.Source.Success("out/varianteffect/snp/")
 
   /** Ouputs to watch. */
-  override val sources: Seq[Input.Source] = Seq(bottomLine)
+  override val sources: Seq[Input.Source] = Seq(ancestrySpecific, mixedDatasets)
 
   /** Process top associations for each phenotype. */
   override val rules: PartialFunction[Input, Outputs] = {
-    case bottomLine(phenotype) => Outputs.Named(phenotype)
+    case ancestrySpecific(phenotype, ancestry) => Outputs.Named(s"$phenotype/${ancestry.split("=").last}")
+    case mixedDatasets(_, _, phenotype) => Outputs.Named(s"$phenotype/Mixed")
   }
 
   /** Simple cluster with more memory. */
   override val cluster: ClusterDef = super.cluster.copy(
-    masterInstanceType = Strategy.generalPurpose(mem = 64.gb),
-    slaveInstanceType = Strategy.generalPurpose(mem = 32.gb),
-    instances = 4
+    instances = 1,
+    bootstrapScripts = Seq(new BootstrapScript(resourceUri("installPythonPackages.sh")))
   )
 
   /** Build the job. */
   override def make(output: String): Job = {
-    val script    = resourceUri("variantAssociations.py")
-    val phenotype = output
+    val input = VariantAssociationsInput.fromString(output)
+    new Job(Job.PySpark(resourceUri("variantAssociations.py"), input.flags:_*))
+  }
 
-    new Job(Job.PySpark(script, phenotype))
+  /** Before the jobs actually run, perform this operation.
+   */
+  override def prepareJob(output: String): Unit = {
+    val input = VariantAssociationsInput.fromString(output)
+    context.s3.rm(input.outputDirectory + "/")
+  }
+
+  /** On success, write the _SUCCESS file in the output directory.
+   */
+  override def success(output: String): Unit = {
+    val input = VariantAssociationsInput.fromString(output)
+    context.s3.touch(input.outputDirectory + "/_SUCCESS")
+    ()
+  }
+}
+
+case class VariantAssociationsInput(
+  phenotype: String,
+  ancestry: String
+) {
+  def outputDirectory: String = s"out/magma/variant-associations/$phenotype/ancestry=$ancestry"
+
+  def flags: Seq[String] = Seq(s"--phenotype=$phenotype", s"--ancestry=$ancestry")
+}
+
+object VariantAssociationsInput {
+  def fromString(output: String): VariantAssociationsInput = {
+    output.split("/").toSeq match {
+      case Seq(phenotype, ancestry) => VariantAssociationsInput(phenotype, ancestry)
+    }
   }
 }
