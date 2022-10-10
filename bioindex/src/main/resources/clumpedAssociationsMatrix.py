d79f6e4805b077be60d7e4bcc253bd40a717b85d
psmadbec <psmadbec@broadinstitute.org>
2022-10-10 11:26:04 -0400

    [Clumped Variants and Clumped Matrix] Ancestry-specific work

--- bioindex/src/main/resources/clumpedAssociationsMatrix.py
+++ bioindex/src/main/resources/clumpedAssociationsMatrix.py
@@ -1,37 +1,57 @@
 import argparse
 
 from pyspark.sql import SparkSession, DataFrame
+from pyspark.sql.functions import lit
+
+
+def get_clump_df(spark, clumpdir):
+    clump_df = spark.read.json(clumpdir)
+    # limit the data being written
+    clump_df = clump_df.select(
+        clump_df.phenotype.alias('leadPhenotype'),
+        clump_df.clump,
+        clump_df.varId,
+        clump_df.alignment,
+    )
+    return clump_df
+
+
+def get_assocs_df(spark, assocsdir):
+    assocs_df = spark.read.json(assocsdir)
+    assocs_df = assocs_df.filter(assocs_df.pValue <= 0.05)
+    return assocs_df
 
 
 def main():
     """
-    Arguments: none
+    Arguments:  ancestry - str indicating which ancestry to run the analysis against
     """
+    opts = argparse.ArgumentParser()
+    opts.add_argument('--ancestry', type=str, required=True)
+    args = opts.parse_args()
+
     spark = SparkSession.builder.appName('bioindex').getOrCreate()
 
-    # load and output directory
-    srcdir = 's3://dig-analysis-data/out/metaanalysis'
-    common_dir = 's3://dig-analysis-data/out/varianteffect/common'
-    outdir = 's3://dig-bio-index/associations/matrix'
+    # source and output locations
+    s3_bucket = 'dig-bio-index'
+    if args.ancestry == 'Mixed':
+        clumpdir = f's3://dig-analysis-data/out/metaanalysis/clumped/*/part-*'
+        assocsdir = f's3://dig-analysis-data/out/metaanalysis/trans-ethnic/*/part-*'
+        outdir = f's3://{s3_bucket}/associations/matrix'
+    else:
+        clumpdir = f's3://dig-analysis-data/out/metaanalysis/ancestry-clumped/*/ancestry={args.ancestry_specific}/part-*'
+        assocsdir = f's3://dig-analysis-data/out/metaanalysis/ancestry-specific/*/ancestry={args.ancestry_specific}/part-*'
+        outdir = f's3://{s3_bucket}/ancestry-associations/matrix/{args.ancestry_specific}'
 
-    # load the top association clumps
-    clumps = spark.read.json(f'{srcdir}/clumped/*/part-*')
+    clumps = get_clump_df(spark, clumpdir) \
+        .withColumn('ancestry', lit(args.ancestry_specific))
+    assocs = get_assocs_df(spark, assocsdir)
+
+    common_dir = 's3://dig-analysis-data/out/varianteffect/common'
     common = spark.read.json(f'{common_dir}/part-*')
 
-    # load associations across all phenotypes
-    assocs = spark.read.json(f'{srcdir}/trans-ethnic/*/part-*')
-    assocs = assocs.filter(assocs.pValue <= 0.05)
-
-    # drop the existing association and common fields to prevent duplication
-    df = clumps.select(
-        clumps.phenotype.alias('leadPhenotype'),
-        clumps.clump,
-        clumps.varId,
-        clumps.alignment,
-    )
-
     # join to build the associations matrix
-    df = df.join(assocs, on='varId', how='inner')
+    df = clumps.join(assocs, on='varId', how='inner')
     df = df.filter(df.phenotype != df.leadPhenotype)
 
     # per clump, keep only the best association per phenotype
--- bioindex/src/main/resources/clumpedVariants.py
+++ bioindex/src/main/resources/clumpedVariants.py
@@ -1,21 +1,32 @@
 import argparse
 
 from pyspark.sql import SparkSession, DataFrame
+from pyspark.sql.functions import lit
 
 
 def main():
     """
-    Arguments: none
+    Arguments:  ancestry- str indicating which ancestry to run the analysis against
     """
+    opts = argparse.ArgumentParser()
+    opts.add_argument('--ancestry', type=str, required=True)
+    args = opts.parse_args()
+
     spark = SparkSession.builder.appName('bioindex').getOrCreate()
 
-    # load and output directory
-    srcdir = 's3://dig-analysis-data/out/metaanalysis/clumped'
-    common_dir = 's3://dig-analysis-data/out/varianteffect/common'
-    outdir = 's3://dig-bio-index/associations/clump'
+    # source and output locations
+    s3_bucket = 'dig-bio-index'
+    if args.ancestry == 'Mixed':
+        srcdir = f's3://dig-analysis-data/out/metaanalysis/clumped/*/part-*'
+        outdir = f's3://{s3_bucket}/associations/clump'
+    else:
+        srcdir = f's3://dig-analysis-data/out/metaanalysis/ancestry-clumped/*/ancestry={args.ancestry_specific}/part-*'
+        outdir = f's3://{s3_bucket}/ancestry-associations/clump/{args.ancestry_specific}'
 
-    # load the top association clumps
-    clumps = spark.read.json(f'{srcdir}/*/part-*')
+    clumps = spark.read.json(srcdir)\
+        .withColumn('ancestry', lit(args.ancestry_specific))
+
+    common_dir = 's3://dig-analysis-data/out/varianteffect/common'
     common = spark.read.json(f'{common_dir}/part-*')
 
     # join to get and common fields
