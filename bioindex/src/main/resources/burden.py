97ae95a46795df0e2e004c385255776af9f06f92
psmadbec <psmadbec@broadinstitute.org>
2022-10-13 12:25:56 -0400

    [Transcript] Binning code change to allow for transcript search

--- bioindex/src/main/resources/burden.py
+++ bioindex/src/main/resources/burden.py
@@ -1,29 +1,41 @@
+import argparse
 from pyspark.sql import SparkSession
+
+
+key_map = {
+    'gene': 'geneSymbol',
+    'transcript': 'transcriptId'
+}
 
 
 def main():
     """
     Arguments: none
     """
+    opts = argparse.ArgumentParser()
+    opts.add_argument('run_type')
+    args = opts.parse_args()
+
     spark = SparkSession.builder.appName('bioindex').getOrCreate()
 
     # load and output directory
-    srcdir = f's3://dig-analysis-data/out/burdenbinning/*'
+    srcdir = f's3://dig-analysis-data/out/burdenbinning/{args.run_type}/*'
     outdir = f's3://dig-bio-index/burden'
+    key = key_map[args.run_type]
 
     df = spark.read.json(f'{srcdir}/part-*')
 
     # sort by gene and then bin
-    df.orderBy(['geneSymbol', 'burdenBinId']) \
+    df.orderBy([key, 'burdenBinId']) \
         .write \
         .mode('overwrite') \
-        .json('%s/gene' % outdir)
+        .json(f'{outdir}/{args.run_type}')
 
     # sort by datatype, gene and then bin
-    df.orderBy(['datatype', 'geneSymbol', 'burdenBinId']) \
+    df.orderBy(['datatype', key, 'burdenBinId']) \
         .write \
         .mode('overwrite') \
-        .json('%s/datatype' % outdir)
+        .json(f'{outdir}/datatype/{args.run_type}')
 
     # done
     spark.stop()
