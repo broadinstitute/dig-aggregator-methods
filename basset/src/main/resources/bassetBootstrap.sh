de2de8d412d2cf817b7dc1a6c54bc721e741cf52
psmadbec <psmadbec@broadinstitute.org>
2022-11-15 12:15:01 -0500

    [Basset] Modify to modernize

--- basset/src/main/resources/bassetBootstrap.sh
+++ basset/src/main/resources/bassetBootstrap.sh
@@ -1,37 +1,17 @@
 #!/bin/bash -xe
 
-# Bootstrap scripts can either be run as a...
-#
-#  bootstrapScript
-#  bootstrapStep
-#
-# A bootstrap script is run while the machine is being provisioned by
-# AWS, are run as a different user, and must complete within 60 minutes
-# or the provisioning fails. This can be a good thing, as it prevents
-# accidentally creating scripts that never terminate (e.g. waiting for
-# user input).
-#
-# A bootstrap step is a "step" like any other job step. It can take as
-# long as needed. It is run as the hadoop user and is run in the step's
-# directory (e.g. /mnt/var/lib/hadoop/steps/s-123456789).
-#
-# Most of the time, it's best to user a bootstrap script and not step.
+# need to upgrade pip
+sudo python3 -m pip install --upgrade pip
 
-#sudo yum groups mark convert
-#
-## check if GCC, make, etc. are installed already
-#DEVTOOLS=$(sudo yum grouplist | grep 'Development Tools')
-#
-#if [ -z "$DEVTOOLS" ]; then
-#    sudo yum groupinstall -y 'Development Tools'
-#fi
-
+# upgrading pip moves it to a location root doesn't have in its PATH
+sudo ln -s /usr/local/bin/pip /bin/pip
+sudo ln -s /usr/local/bin/pip3 /bin/pip3
 
 # install the python libraries
 sudo pip3 install torch==1.5.1
 sudo pip3 install twobitreader
 sudo pip3 install numpy
-sudo pip3 install sklearn
+sudo pip3 install scikit-learn
 
 # create the work dir
 WORK_DIR="/mnt/var/basset"
