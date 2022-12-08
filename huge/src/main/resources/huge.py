5bb5d487bff83aaad0e9741fb465526664a173c0
curoli <curoli@gmail.com>
2022-12-08 17:36:06 -0500

    Minor.

--- huge/src/main/resources/huge.py
+++ huge/src/main/resources/huge.py
@@ -1,12 +1,29 @@
 import argparse
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, DataFrame
+from datetime import datetime
+
+
+def now_str():
+    return str(datetime.now())
+
+
+def inspect_df(df: DataFrame, name: str):
+    n_rows = df.count()
+    print("Dataframe ", name, " has ", n_rows, " rows.", "(", now_str(), ")")
+    df.printSchema()
+    n_rows_max = 23
+    if n_rows > n_rows_max:
+        df.sample(fraction=(n_rows_max / n_rows)).show()
+    else:
+        df.show()
+    print('Done showing ', name, ' at ', now_str())
 
 
 def main():
     """
     Arguments: none
     """
-    print('Hello!')
+    print('Hello! The time is now ', now_str())
     print('Now building argument parser')
     arg_parser = argparse.ArgumentParser(prog='huge.py')
     arg_parser.add_argument("--phenotype", help="The phenotype", required=True)
@@ -15,6 +32,8 @@ def main():
     arg_parser.add_argument("--variants", help="Variant data", required=True)
     arg_parser.add_argument("--padding", help="Variants are considered this far away from the gene", type=int,
                             default=100000)
+    arg_parser.add_argument("--cqs", help="Variant CQS data", required=True)
+    arg_parser.add_argument("--effects", help="Variant effect data", required=True)
     print('Now parsing CLI arguments')
     cli_args = arg_parser.parse_args()
     phenotype = cli_args.phenotype
@@ -22,43 +41,39 @@ def main():
     genes_glob = cli_args.genes + files_glob
     genes_assoc_glob = cli_args.gene_associations + files_glob
     variants_glob = cli_args.variants + files_glob
+    variant_cqs_glob = cli_args.cqs + files_glob
+    variant_effects_glob = cli_args.effects + files_glob
     padding = cli_args.padding
-    print('Phenotype: ' + phenotype)
-    print('Genes data with regions: ' + genes_glob)
-    print('Gene data with p-values: ' + genes_assoc_glob)
-    print('Variant data: ' + variants_glob)
-    print('Padding: ' + str(padding))
+    print('Phenotype: ', phenotype)
+    print('Genes data with regions: ', genes_glob)
+    print('Gene data with p-values: ', genes_assoc_glob)
+    print('Variant data: ', variants_glob)
+    print('Variant CQS data: ', variant_cqs_glob)
+    print('Variant effects data: ', variant_effects_glob)
+    print('Padding: ', padding)
     spark = SparkSession.builder.appName('huge').getOrCreate()
-    print('Genes from ' + genes_glob + ':')
     genes_regions_raw = spark.read.json(genes_glob)
     gene_regions = genes_regions_raw.select('chromosome', 'start', 'end', 'source', 'name') \
         .filter(genes_regions_raw.source == 'symbol').drop(genes_regions_raw.source)
-    print('There are ' + str(gene_regions.count()) + ' gene regions:')
-    for row in gene_regions.take(42):
-        print(row)
+    inspect_df(gene_regions, "gene regions")
     gene_p_values = spark.read.json(genes_assoc_glob).select('gene', 'pValue')
-    print('There are ' + str(gene_p_values.count()) + ' gene associations')
-    for row in gene_p_values.take(42):
-        print(row)
+    inspect_df(gene_p_values, "gene associations")
     genes = gene_regions.join(gene_p_values, gene_regions.name == gene_p_values.gene).drop(gene_regions.name) \
         .withColumnRenamed('chromosome', 'chromosome_gene').withColumnRenamed('pValue', 'pValue_gene')
-    print("Joined gene data gives " + str(genes.count()) + ' rows:')
-    for row in genes.take(42):
-        print(row)
+    inspect_df(genes, "joined genes data")
     variants = spark.read.json(variants_glob).select('chromosome', 'position', 'reference', 'alt', 'pValue')
-    print('There is data from ' + str(variants.count()) + ' variants:')
-    for row in variants.take(42):
-        print(row)
+    inspect_df(variants, "variants for phenotype")
     cond = (genes.chromosome_gene == variants.chromosome) & \
            (genes.start - padding <= variants.position) & \
            (genes.end + padding >= variants.position)
     gene_variants = genes.join(variants.alias('variants'), cond, "inner").select('gene', 'pValue_gene', 'pValue')
-    print('Joining genes and variants give ' + str(gene_variants.count()) + ' pairs:')
-    for row in gene_variants.take(42):
-        print(row)
+    inspect_df(gene_variants, "joined genes and variants")
     grouped = gene_variants.groupBy(gene_variants.gene, gene_variants.pValue_gene).agg({"pValue": "min"})
-    for row in grouped.take(42):
-        print(row)
+    inspect_df(grouped, "variants grouped by gene and aggregated")
+    variant_cqs = spark.read.json(variant_cqs_glob).select("consequenceTerms")
+    inspect_df(variant_cqs, "CQS")
+    variant_effects = spark.read.json(variant_effects_glob).select("consequenceTerms")
+    inspect_df(variant_effects, "variant effects")
     print('Stopping Spark')
     spark.stop()
 
--- huge/src/main/scala/org/broadinstitute/dig/aggregator/methods/huge/HugeStage.scala
+++ huge/src/main/scala/org/broadinstitute/dig/aggregator/methods/huge/HugeStage.scala
@@ -21,30 +21,28 @@ class HugeStage(implicit context: Context) extends Stage {
 
   class FilesForPhenotype(string: String, placeHolder: String = "<phenotype>") {
     def forPhenotype(phenotype: String): String = string.replaceAll(placeHolder, phenotype)
-    def asGlob: String = string.replaceAll(placeHolder, "*")
+    def asGlob: String                          = string.replaceAll(placeHolder, "*")
   }
 
-  val geneFile: String = "genes/GRCh37/"
+  val geneFile: String                  = "genes/GRCh37/"
   val geneAssocFiles: FilesForPhenotype = new FilesForPhenotype("gene_associations/52k_*/<phenotype>/")
-  val variantFiles: FilesForPhenotype = new FilesForPhenotype("out/metaanalysis/trans-ethnic/<phenotype>/")
+  val variantFiles: FilesForPhenotype   = new FilesForPhenotype("out/metaanalysis/trans-ethnic/<phenotype>/")
+  val variantCqsFiles: String           = "out/varianteffect/cqs/"
+  val variantEffectFiles: String        = "out/varianteffect/effects/"
 
   val genes: Input.Source            = Input.Source.Dataset(geneFile)
   val geneAssociations: Input.Source = Input.Source.Dataset(geneAssocFiles.asGlob)
   val variants: Input.Source         = Input.Source.Success(variantFiles.asGlob)
+  val variantCqs: Input.Source       = Input.Source.Success(variantCqsFiles)
+  val variantEffects: Input.Source   = Input.Source.Success(variantEffectFiles)
 
   /** Source inputs. */
-  override val sources: Seq[Input.Source] = Seq(genes, geneAssociations, variants)
-
-  val makeClusterSmall: Boolean = true
+  override val sources: Seq[Input.Source] = Seq(genes, geneAssociations, variants, variantCqs, variantEffects)
 
   /* Define settings for the cluster to run the job.
    */
   override val cluster: ClusterDef = {
-    if (makeClusterSmall) {
-      super.cluster.copy(instances = 1)
-    } else {
-      super.cluster.copy()
-    }
+    super.cluster.copy(instances = 3)
   }
 
   /** Map inputs to outputs. */
@@ -52,6 +50,8 @@ class HugeStage(implicit context: Context) extends Stage {
     case genes()                        => Outputs.All
     case geneAssociations(_, phenotype) => Outputs.Named(phenotype)
     case variants(phenotype)            => Outputs.Named(phenotype)
+    case variantCqs()                   => Outputs.All
+    case variantEffects()               => Outputs.All
   }
 
   /** One job per phenotype (e.g. T2D)
@@ -61,10 +61,22 @@ class HugeStage(implicit context: Context) extends Stage {
     val phenotype = output
     println(s"Making job with script $script for phenotype $phenotype.")
     val bucket = context.s3
-    new Job(Job.PySpark(script,
-      "--phenotype", phenotype,
-      "--genes", bucket.s3UriOf(geneFile).toString,
-      "--gene-associations", bucket.s3UriOf(geneAssocFiles.forPhenotype(phenotype)).toString,
-      "--variants", bucket.s3UriOf(variantFiles.forPhenotype(phenotype)).toString))
+    new Job(
+      Job.PySpark(
+        script,
+        "--phenotype",
+        phenotype,
+        "--genes",
+        bucket.s3UriOf(geneFile).toString,
+        "--gene-associations",
+        bucket.s3UriOf(geneAssocFiles.forPhenotype(phenotype)).toString,
+        "--variants",
+        bucket.s3UriOf(variantFiles.forPhenotype(phenotype)).toString,
+        "--cqs",
+        bucket.s3UriOf(variantCqsFiles).toString,
+        "--effects",
+        bucket.s3UriOf(variantEffectFiles).toString
+      )
+    )
   }
 }
