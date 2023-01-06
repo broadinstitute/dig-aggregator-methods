f0eede474093d8e535f34025298f516d74a2b55c
psmadbec <psmadbec@broadinstitute.org>
2023-01-06 10:29:19 -0500

    bottom-line change

--- bioindex/src/main/scala/DatasetAssociationsStage.scala
+++ bioindex/src/main/scala/DatasetAssociationsStage.scala
@@ -25,7 +25,7 @@ class DatasetAssociationsStage(implicit context: Context) extends Stage {
     masterInstanceType = Ec2.Strategy.memoryOptimized(),
     masterVolumeSizeInGB = 200,
     bootstrapScripts = Seq(new BootstrapScript(resourceUri("cluster-bootstrap-6.7.0.sh"))),
-    releaseLabel = ReleaseLabel("emr-6.7.0") // Need emr 6.1+ to write json files properly
+    releaseLabel = ReleaseLabel("emr-6.7.0") // Need emr 6.1+ to read zstd files
   )
 
   /** Output to Job steps. */
