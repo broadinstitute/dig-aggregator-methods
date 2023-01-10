ec677e3e60bf4be991e1aadc54a20ad30bc1b879
curoli <curoli@gmail.com>
2023-01-10 11:10:56 -0500

    conda needs --yes in batch

--- huge/src/main/resources/huge-bootstrap.sh
+++ huge/src/main/resources/huge-bootstrap.sh
@@ -6,5 +6,5 @@ sudo yum install -y python3-devel
 python3 --version
 wget https://repo.anaconda.com/miniconda/Miniconda3-py37_22.11.1-1-Linux-x86_64.sh
 bash Miniconda3-py37_22.11.1-1-Linux-x86_64.sh -b
-/home/hadoop/miniconda3/bin/conda install scipy
+/home/hadoop/miniconda3/bin/conda install scipy --yes
 echo "Yoohoo, done bootstrapping."
