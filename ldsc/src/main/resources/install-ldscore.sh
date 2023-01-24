fbecfedcf7d7ccaee754bf23f7e1da016e848cd5
psmadbec <psmadbec@broadinstitute.org>
2023-01-24 14:49:46 -0500

    [LDSC] Region to Annot Stage

---
+++ ldsc/src/main/resources/install-ldscore.sh
@@ -0,0 +1,61 @@
+#!/bin/bash -xe
+
+## NOTE: This uses a python 3 version of ldsc which is saved as a zip in s3
+## Developed with python 3.8.12 installed
+
+LDSC_ROOT=/mnt/var/ldsc
+
+# install to the root directory
+sudo mkdir -p "$LDSC_ROOT"
+cd "$LDSC_ROOT"
+
+# Download zipped version of python 3 ldsc codebase
+sudo aws s3 cp s3://dig-analysis-data/bin/ldsc/ldsc-python-3-2022-09-06.zip ./
+sudo unzip ldsc-python-3-2022-09-06.zip -d ./ldsc/
+
+## Download hapmap3 snps (full)
+# From https://data.broadinstitute.org/alkesgroup/LDSCORE/w_hm3.snplist.bz2 downloaded September 9, 2022
+sudo aws s3 cp s3://dig-analysis-data/bin/ldsc/w_hm3.snplist.bz2 ./
+sudo bunzip2 w_hm3.snplist.bz2
+sudo mkdir ./snps
+sudo mv w_hm3.snplist ./snps/
+
+## Download hapmap3 snps (chr specific)
+# From https://data.broadinstitute.org/alkesgroup/LDSCORE/hapmap3_snps.tgz downloaded January 20, 2023
+sudo aws s3 cp s3://dig-analysis-data/bin/ldsc/hapmap3_snps.tgz ./
+sudo tar -xf hapmap3_snps.tgz
+sudo mv hapmap3_snps/* ./snps/
+sudo rm -r hapmap3_snps
+sudo rm hapmap3_snps.tgz
+
+# install dependencies
+sudo yum install -y python3-devel
+sudo yum install -y bzip2-devel
+sudo pip3 install -U bitarray
+sudo pip3 install -U boto3
+sudo pip3 install -U sqlalchemy
+sudo pip3 install -U pymysql
+sudo pip3 install -U Cython
+sudo pip3 install -U pybind11
+sudo pip3 install -U pythran
+sudo pip3 install -U scipy
+sudo pip3 install -U pandas
+sudo pip3 install -U pybedtools
+
+# install BEDTools
+sudo wget https://github.com/arq5x/bedtools2/releases/download/v2.30.0/bedtools-2.30.0.tar.gz
+sudo tar -zxvf bedtools-2.30.0.tar.gz
+cd bedtools2
+sudo make
+sudo cp bin/* /usr/local/bin/
+cd "$LDSC_ROOT"
+
+# g1000 datasets
+sudo mkdir -p ./g1000
+for ANCESTRY in AFR AMR EAS EUR SAS
+do
+  sudo mkdir -p ./g1000/$ANCESTRY
+  sudo aws s3 cp s3://dig-analysis-data/bin/ldsc/g1000/g1000_chr_$ANCESTRY.zip ./
+  sudo unzip g1000_chr_$ANCESTRY.zip -d ./g1000/$ANCESTRY/
+  sudo rm g1000_chr_$ANCESTRY.zip
+done
---
+++ ldsc/src/main/scala/RegionToAnnotStage.scala
@@ -0,0 +1,47 @@
+package org.broadinstitute.dig.aggregator.methods.ldsc
+
+import org.broadinstitute.dig.aggregator.core._
+import org.broadinstitute.dig.aws.emr._
+
+class RegionToAnnotStage(implicit context: Context) extends Stage {
+
+  val partitions: Seq[String] = Seq()
+  val subRegion: String = if (partitions.isEmpty) "default" else partitions.mkString("-")
+  val mergedFiles: Input.Source = Input.Source.Success(s"out/ldsc/regions/${subRegion}/merged/*/")
+
+  /** Source inputs. */
+  override val sources: Seq[Input.Source] = Seq(mergedFiles)
+
+  /** Just need a single machine with no applications, but a good drive. */
+  override def cluster: ClusterDef = super.cluster.copy(
+    instances = 1,
+    applications = Seq.empty,
+    bootstrapScripts = Seq(new BootstrapScript(resourceUri("install-ldscore.sh"))),
+    releaseLabel = ReleaseLabel("emr-6.7.0"),
+    stepConcurrency = 5
+  )
+
+  override val rules: PartialFunction[Input, Outputs] = {
+    case mergedFiles(region) => Outputs.Named(region)
+  }
+
+  /** The partition names are combined together across datasets into single
+    * BED files that can then be read by GREGOR.
+    */
+  override def make(output: String): Job = {
+    new Job(Job.Script(resourceUri("regionsToAnnot.py"), s"--sub-region=$subRegion", s"--region-name=$output"))
+  }
+
+  /** Before the jobs actually run, perform this operation.
+    */
+  override def prepareJob(output: String): Unit = {
+    context.s3.rm(s"out/ldsc/regions/${subRegion}/annot/${output}/")
+  }
+
+  /** Update the success flag of the merged regions.
+    */
+  override def success(output: String): Unit = {
+    context.s3.touch(s"out/ldsc/regions/${subRegion}/annot/${output}/_SUCCESS")
+    ()
+  }
+}
