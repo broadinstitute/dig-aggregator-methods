dd2b3571c9c6e995e6288da5f62b6ae002b9d050
curoli <curoli@gmail.com>
2023-02-15 18:46:24 -0500

    Optimization by persisting/unpersisting.

--- gene-id-map/src/main/resources/gene-id-map.py
+++ gene-id-map/src/main/resources/gene-id-map.py
@@ -6,13 +6,15 @@ from pyspark.sql.functions import col, udf, when
 from pyspark.sql.types import StringType
 
 
-@udf(returnType=StringType())
-def symbol_to_ensembl(symbol: str) -> str:
+def symbol_to_ensembl_impl(symbol: str) -> str:
     cmd = ('eugene', 'util', 'symbol-to-gene-id', '-s', symbol)
     process = subprocess.run(cmd, capture_output=True, text=True)
     ensembl = process.stdout.strip()
     print("Mapped symbol", symbol, "to Ensembl id ", ensembl)
     return ensembl
+
+
+symbol_to_ensembl = udf(lambda symbol: symbol_to_ensembl_impl(symbol), StringType()).asNondeterministic()
 
 
 def now_str():
@@ -53,34 +55,41 @@ def main():
     print('Variant effects data dir: ', variant_effects_glob)
     print('Id map output dir: ', map_dir)
     spark = SparkSession.builder.appName('geneidmap').getOrCreate()
-    genes = spark.read.json(genes_glob).select("chromosome", "start", "end", "name", "source")
-    genes_symbol = \
-        genes.filter(genes.source == "symbol") \
-            .select("chromosome", "start", "end", "name") \
-            .withColumnRenamed("name", "symbol")
-    genes_ensembl = \
-        genes.filter(genes.source == "ensembl") \
-            .select("chromosome", "start", "end", "name") \
-            .withColumnRenamed("name", "ensembl")
-    genes_joined = genes_symbol.join(genes_ensembl, ["chromosome", "start", "end"])
-    genes_joined.write.mode("overwrite").json(genes_out_dir)
+    # genes = spark.read.json(genes_glob).select("chromosome", "start", "end", "name", "source")
+    # genes_symbol = \
+    #     genes.filter(genes.source == "symbol") \
+    #         .select("chromosome", "start", "end", "name") \
+    #         .withColumnRenamed("name", "symbol")
+    # genes_ensembl = \
+    #     genes.filter(genes.source == "ensembl") \
+    #         .select("chromosome", "start", "end", "name") \
+    #         .withColumnRenamed("name", "ensembl")
+    # genes_joined = genes_symbol.join(genes_ensembl, ["chromosome", "start", "end"])
+    # genes_joined.write.mode("overwrite").json(genes_out_dir)
     symbols = \
         spark.read.json(variant_effects_glob).select("nearest") \
             .withColumn("symbol", col("nearest").getItem(0)).select("symbol").distinct()
     inspect_df(symbols, "Nearest from effects")
-    gene_id_map = symbols.withColumn("ensembl", symbol_to_ensembl(symbols.symbol))
+    gene_id_map = symbols.withColumn("ensembl", symbol_to_ensembl(symbols.symbol)).persist()
     inspect_df(gene_id_map, "gene id map")
     count = 1
+    print("Entering loop")
     while count > 0:
-        gene_id_map_missing = gene_id_map.filter(col("ensembl") == "")
+        gene_id_map_missing = gene_id_map.filter(col("ensembl") == "").persist()
         inspect_df(gene_id_map_missing, "gene id map missing")
         count = gene_id_map_missing.count()
-        gene_id_map = \
+        print("count is", count, "and time is", now_str())
+        gene_id_map_missing.unpersist()
+        gene_id_map_new = \
             gene_id_map.withColumn("ensembl_new",
                                    when(gene_id_map.ensembl == "", symbol_to_ensembl(gene_id_map.symbol))
                                    .otherwise(gene_id_map.ensembl)) \
-                .drop("ensembl").withColumnRenamed("ensembl_new", "ensembl")
+                .drop("ensembl").withColumnRenamed("ensembl_new", "ensembl").persist()
+        gene_id_map.unpersist()
+        gene_id_map = gene_id_map_new
 
+    inspect_df(gene_id_map, "gene id map")
+    print("Exited the loop, now writing gene id map.")
     gene_id_map.write.mode("overwrite").json(map_dir)
     print('Done with work, therefore stopping Spark')
     spark.stop()
