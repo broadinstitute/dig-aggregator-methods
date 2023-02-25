2f28f0395f440e79a90c7f26fc09a658547147a9
curoli <curoli@gmail.com>
2023-02-24 20:10:04 -0500

    Limit genes/variants join to variants within region of gene.

--- nearest-gene/src/main/resources/nearest-gene.py
+++ nearest-gene/src/main/resources/nearest-gene.py
@@ -30,20 +30,29 @@ def main():
     arg_parser = argparse.ArgumentParser(prog='gene-id-map.py')
     arg_parser.add_argument("--genes-dir", help="gene data dir", required=True)
     arg_parser.add_argument("--variants-dir", help="variant data dir", required=True)
+    arg_parser.add_argument("--padding", help="Variants are considered this far away from the gene", type=int,
+                            default=100000)
     arg_parser.add_argument("--out-dir", help="output dir", required=True)
     print('Now parsing CLI arguments')
     cli_args = arg_parser.parse_args()
     files_glob = 'part-*'
     genes_glob = cli_args.genes_dir + files_glob
     variants_glob = cli_args.variants_dir + files_glob
+    padding = cli_args.padding
     out_dir = cli_args.out_dir
     print('Gene data dir: ', genes_glob)
     print('Variant effects data dir: ', variants_glob)
+    print('Padding: ', padding)
     print('Output dir: ', out_dir)
     spark = SparkSession.builder.appName('nearest_gene').getOrCreate()
-    genes = spark.read.json(genes_glob).select("chromosome", "start", "end", "ensembl")
+    genes = spark.read.json(genes_glob).select("chromosome", "start", "end", "ensembl")\
+        .withColumnRenamed("chromosome", "chromosome_gene")
     variants = spark.read.json(variants_glob).select("varId", "chromosome", "position")
-    joined = genes.join(variants, ["chromosome"])
+    variant_in_region = \
+        (genes.chromosome_gene == variants.chromosome) & \
+        (genes.start - padding <= variants.position) & \
+        (genes.end + padding >= variants.position)
+    joined = genes.join(variants, variant_in_region).drop("chromosome_gene")
     distances = \
         joined.withColumn("distance", greatest(col("start") - col("position"), col("position") - col("end"), lit(0)))\
             .withColumn("length", col("end") - col("start"))
