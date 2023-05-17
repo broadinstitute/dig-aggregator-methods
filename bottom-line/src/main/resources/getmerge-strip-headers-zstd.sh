af3192355cf6f977893e5bd16117295920bac7c7
psmadbec <psmadbec@broadinstitute.org>
2023-05-17 16:30:25 -0400

    Review comment

--- bottom-line/src/main/resources/getmerge-strip-headers-zstd.sh
+++ bottom-line/src/main/resources/getmerge-strip-headers-zstd.sh
@@ -14,14 +14,5 @@ aws s3 cp $glob ./tmp_files/ --recursive
 zstd -d --rm ./tmp_files/part-*
 
 # merge files
-cat ./tmp_files/part-* > "$outfile"
+awk '(NR == 1) || (FNR > 1)' ./tmp_files/part-* > "$outfile"
 rm -r tmp_files
-
-# discover the header to skip it later
-header=$(head -n 1 "$outfile")
-
-# where to do temporary file processing
-tmp="$(dirname "$outfile")/tmp.csv"
-
-# run awk to remove extra CSV headers
-awk -v h="$header" 'NR>1 && $0~h {next} {print}' "$outfile" > "$tmp" && mv "$tmp" "$outfile"
