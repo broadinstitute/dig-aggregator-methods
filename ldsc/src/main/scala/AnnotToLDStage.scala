95d3b3c2860d45a839dc8b509b2c8bed2c4c5f27
psmadbec <psmadbec@broadinstitute.org>
2023-06-06 18:04:14 -0400

    stash

--- ldsc/src/main/scala/AnnotToLDStage.scala
+++ ldsc/src/main/scala/AnnotToLDStage.scala
@@ -8,9 +8,7 @@ import org.broadinstitute.dig.aws.Ec2.Strategy
 class AnnotToLDStage(implicit context: Context) extends Stage {
   import MemorySize.Implicits._
 
-  val partitions: Seq[String] = Seq()
-  val subRegion: String = if (partitions.isEmpty) "default" else partitions.mkString("-")
-  val annotFiles: Input.Source = Input.Source.Success(s"out/ldsc/regions/${subRegion}/annot/*/*/")
+  val annotFiles: Input.Source = Input.Source.Success(s"out/ldsc/regions/annot/*/*/*/")
 
   /** Source inputs. */
   override val sources: Seq[Input.Source] = Seq(annotFiles)
@@ -24,27 +22,70 @@ class AnnotToLDStage(implicit context: Context) extends Stage {
     releaseLabel = ReleaseLabel("emr-6.7.0")
   )
 
+  var regionCache: Seq[AnnotToLDRegion] = Seq.empty
+  lazy val regionMap: Map[(String, String), Seq[AnnotToLDRegion]] = regionCache.groupBy(output => output.group.key)
+
   override val rules: PartialFunction[Input, Outputs] = {
-    case annotFiles(ancestry, _) => Outputs.Named(ancestry.split("=").last)
+    case annotFiles(ancestry, subRegion, region) =>
+      val annotToLDRegion = AnnotToLDRegion(ancestry.split("=").last, subRegion, region)
+      regionCache ++= Seq(annotToLDRegion)
+      Outputs.Named(annotToLDRegion.group.toOutput)
   }
 
   /** The partition names are combined together across datasets into single
     * BED files that can then be read by GREGOR.
     */
   override def make(output: String): Job = {
-    new Job(Job.Script(resourceUri("annotToLD.py"), s"--sub-region=$subRegion", s"--ancestry=$output"))
+    val group = AnnotToLDGroup.fromOutput(output)
+    val jobs = regionMap.getOrElse(group.key, Seq()).grouped(100).map { subGroup =>
+      println(s"Adding group for ${group.ancestry} and ${group.subRegion} with ${subGroup.length} regions")
+      Job.Script(
+        resourceUri("annotToLD.py"),
+        s"--ancestry=${group.ancestry}",
+        s"--sub-region=${group.subRegion}",
+        s"--regions=${subGroup.map(_.region).mkString(",")}"
+      )
+    }.toSeq
+    new Job(jobs)
   }
 
   /** Before the jobs actually run, perform this operation.
     */
   override def prepareJob(output: String): Unit = {
-    context.s3.rm(s"out/ldsc/regions/${subRegion}/ld_score/ancestry=${output}/")
+    regionCache.map { region =>
+      context.s3.rm(s"${region.directory}/")
+    }
   }
 
   /** Update the success flag of the merged regions.
     */
   override def success(output: String): Unit = {
-    context.s3.touch(s"out/ldsc/regions/${subRegion}/ld_score/ancestry=${output}/_SUCCESS")
+    regionCache.map { region =>
+      context.s3.rm(s"${region.directory}/_SUCCESS")
+    }
     ()
+  }
+}
+
+case class AnnotToLDRegion(
+  ancestry: String,
+  subRegion: String,
+  region: String
+) {
+  def directory: String = s"out/ldsc/regions/ld_score/ancestry=$ancestry/$subRegion/$region"
+  val group: AnnotToLDGroup = AnnotToLDGroup(ancestry, subRegion)
+}
+
+case class AnnotToLDGroup(
+  ancestry: String,
+  subRegion: String
+) {
+  def key: (String, String) = (ancestry, subRegion)
+  def toOutput: String = s"$ancestry/$subRegion"
+}
+
+object AnnotToLDGroup {
+  def fromOutput(output: String): AnnotToLDGroup = output.split("/").toSeq match {
+    case Seq(ancestry, subRegion) => AnnotToLDGroup(ancestry, subRegion)
   }
 }
