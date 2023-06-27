32690e07977e10d8fc507a7108e3cc51b087e45d
psmadbec <psmadbec@broadinstitute.org>
2023-06-27 14:02:53 -0400

    [Bioindex] Add gene-expression-tissue bioindex

--- bioindex/src/main/resources/geneExpression.py
+++ bioindex/src/main/resources/geneExpression.py
@@ -17,7 +17,29 @@ def main():
     df.orderBy(['gene']) \
         .write \
         .mode('overwrite') \
-        .json(outdir)
+        .json(f'{outdir}/gene')
+
+    # Want to calculate a mean Tpm value for tissue/gene from dataset values
+    aggregate_df = df \
+        .select(['tissue', 'gene', 'meanTpm', 'nSamples'])
+
+    aggregate_df = df \
+        .withColumn('totalTpm', aggregate_df.meanTpm * aggregate_df.nSamples) \
+        .drop('meanTpm') \
+        .groupBy(['tissue', 'gene']) \
+        .agg({'totalTpm': 'sum', 'nSamples': 'sum'}) \
+        .withColumnRenamed('sum(totalTpm)', 'totalTpm') \
+        .withColumnRenamed('sum(nSamples)', 'nSamples')
+
+    aggregate_df = aggregate_df \
+        .withColumn('meanTpm', aggregate_df.totalTpm / aggregate_df.nSamples) \
+        .drop('totalTpm')
+
+    # sort and write
+    aggregate_df.orderBy(['tissue']) \
+        .write \
+        .mode('overwrite') \
+        .json(f'{outdir}/tissue')
 
     # done
     spark.stop()
