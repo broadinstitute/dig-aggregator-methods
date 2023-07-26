d557e5f3fbfa5f7efc57e41647b9a287dac53eaf
psmadbec <psmadbec@broadinstitute.org>
2023-07-26 13:10:56 -0400

    stash

--- bottom-line/src/main/resources/runPlink.py
+++ bottom-line/src/main/resources/runPlink.py
@@ -381,7 +381,10 @@ def main():
         maybe_ancestry = db.get_dataset_ancestry(args.dataset)
         if maybe_ancestry is not None:
             srcdir, plinkdir, outdir = get_dataset_paths(args)
-            ancestries = {maybe_ancestry: ANCESTRY_SPECIFIC_ANCESTRIES[maybe_ancestry]}
+            if args.ancestry == 'Mixed':
+                ancestries = TRANS_ETHNIC_ANCESTRIES
+            else:
+                ancestries = {maybe_ancestry: ANCESTRY_SPECIFIC_ANCESTRIES[maybe_ancestry]}
         else:
             raise Exception(f'No ancestry set for dataset {args.dataset}')
 
