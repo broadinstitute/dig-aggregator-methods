2ad60c5d1a774e2b1ca2d6121c4bd4538c3f7991
psmadbec <psmadbec@broadinstitute.org>
2023-09-18 07:06:07 -0400

    Annot To LD

--- ldsc/src/main/resources/annotToLD.py
+++ ldsc/src/main/resources/annotToLD.py
@@ -16,29 +16,22 @@ snp_files = f'{downloaded_files}/snps'
 s3_in = 's3://dig-analysis-data'
 s3_out = 's3://dig-analysis-data'
 
-CPUs = 16  # For multithreaded combining and splitting annot file
+CPUs = 8  # For multithreaded combining and splitting annot file
 
 
-def get_all_annot_file(sub_region, ancestry):
-    file = f'{s3_in}/out/ldsc/regions/{sub_region}/annot/ancestry={ancestry}/'
-    subprocess.check_call(['aws', 's3', 'cp', file, f'./{ancestry}/', '--recursive'])
-    for file in glob.glob(f'./{ancestry}/*/_SUCCESS'):
-        os.remove(file)
-
-
-def get_region_names(ancestry):
-    chr_1_files = f'./{ancestry}/*/*.1.annot.gz'
-    region_pattern = f'.*/([^/]*)\\.1\\.annot\\.gz'
-    return [re.findall(region_pattern, file)[0] for file in glob.glob(chr_1_files)]
+def get_all_annot_file(ancestry, sub_region, regions):
+    for region in regions:
+        file = f'{s3_in}/out/ldsc/regions/annot/ancestry={ancestry}/{sub_region}/{region}/'
+        subprocess.check_call(['aws', 's3', 'cp', file, f'./{ancestry}/{sub_region}/{region}/', '--recursive', '--exclude=_SUCCESS'])
 
 
 def combine_chr_annot(params):
-    region_names, ancestry, CHR = params
+    ancestry, sub_region, regions, CHR = params
     with gzip.open(f'./{ancestry}.combined.{CHR}.annot.gz', 'w') as f_out:
-        files = [gzip.open(f'./{ancestry}/{region_name}/{region_name}.{CHR}.annot.gz', 'r') for region_name in region_names]
+        files = [gzip.open(f'./{ancestry}/{sub_region}/{region}/{region}.{CHR}.annot.gz', 'r') for region in regions]
         for file in files:
             file.readline()
-        f_out.write('\t'.join(region_names).encode() + b'\n')
+        f_out.write('\t'.join(regions).encode() + b'\n')
         lines = [file.readline().strip() for file in files]
         while len(lines[0]) > 0:
             f_out.write(b'\t'.join(lines) + b'\n')
@@ -47,10 +40,9 @@ def combine_chr_annot(params):
             file.close()
 
 
-def combine_all_annot(ancestry):
-    region_names = get_region_names(ancestry)
+def combine_all_annot(ancestry, sub_region, regions):
     with Pool(CPUs) as p:
-        p.map(combine_chr_annot, [(region_names, ancestry, CHR) for CHR in range(1, 23)])
+        p.map(combine_chr_annot, [(ancestry, sub_region, regions, CHR) for CHR in range(1, 23)])
 
 
 def make_ld(ancestry, CHR):
@@ -69,11 +61,11 @@ def make_ld(ancestry, CHR):
 
 
 def split_ld_file(args):
-    i, ancestry, CHR = args
+    i, ancestry, sub_region, regions, CHR = args
     print(f'splitting file {i} for CHR {CHR} and ancestry {ancestry}')
     with gzip.open(f'./{ancestry}.combined.{CHR}.l2.ldscore.gz', 'r') as f_ld:
-        region_names = [header[:-2] for header in f_ld.readline().decode().strip().split('\t')[3:]]
-        files = [f'./{ancestry}/{region_name}/{region_name}.{CHR}.l2.ldscore.gz' for region_name in region_names]
+        f_ld.readline() # Useless header
+        files = [f'./{ancestry}/{sub_region}/{region}/{region}.{CHR}.l2.ldscore.gz' for region in regions]
         with gzip.open(files[i], 'w') as f:
             f.write(b'CHR\tSNP\tBP\tL2\n')
             lines = f_ld.readline().strip().split(b'\t')
@@ -83,61 +75,63 @@ def split_ld_file(args):
             f.close()
 
 
-def split_ld(ancestry, CHR):
-    with gzip.open(f'./{ancestry}.combined.{CHR}.l2.ldscore.gz', 'r') as f_ld:
-        region_names = [header[:-2] for header in f_ld.readline().decode().strip().split('\t')[3:]]
-
+def split_ld(ancestry, sub_region, regions, CHR):
     print(f'Splitting ld file for ancestry {ancestry} and chromosome {CHR}')
     with Pool(CPUs) as p:
-        p.map(split_ld_file, [(i, ancestry, CHR) for i in range(len(region_names))])
+        p.map(split_ld_file, [(i, ancestry, sub_region, regions, CHR) for i in range(len(regions))])
 
     os.remove(f'./{ancestry}.combined.{CHR}.l2.ldscore.gz')
-    split_m(ancestry, CHR, region_names)
+    split_m(ancestry, sub_region, regions, CHR)
 
 
-def split_m(ancestry, CHR, region_names):
+def split_m(ancestry, sub_region, regions, CHR):
     with open(f'./{ancestry}.combined.{CHR}.l2.M', 'r') as f_M:
         M_values = f_M.readline().strip().split('\t')
     with open(f'./{ancestry}.combined.{CHR}.l2.M_5_50', 'r') as f_M_50:
         M_50_values = f_M_50.readline().strip().split('\t')
-    for i, region_name in enumerate(region_names):
-        with open(f'./{ancestry}/{region_name}/{region_name}.{CHR}.l2.M', 'w') as f:
+    for i, region in enumerate(regions):
+        with open(f'./{ancestry}/{sub_region}/{region}/{region}.{CHR}.l2.M', 'w') as f:
             f.write(M_values[i] + '\n')
-        with open(f'./{ancestry}/{region_name}/{region_name}.{CHR}.l2.M_5_50', 'w') as f:
+        with open(f'./{ancestry}/{sub_region}/{region}/{region}.{CHR}.l2.M_5_50', 'w') as f:
             f.write(M_50_values[i] + '\n')
     os.remove(f'./{ancestry}.combined.{CHR}.l2.M')
     os.remove(f'./{ancestry}.combined.{CHR}.l2.M_5_50')
 
 
-def make_ancestry_ld(ancestry):
+def make_ancestry_ld(ancestry, sub_region, regions):
     for CHR in range(1, 23):
         make_ld(ancestry, CHR)
-        split_ld(ancestry, CHR)
+        split_ld(ancestry, sub_region, regions, CHR)
 
 
-def upload_and_remove_files(sub_region, ancestry):
-    s3_dir = f'{s3_out}/out/ldsc/regions/{sub_region}/ld_score/ancestry={ancestry}/'
-    subprocess.check_call(['aws', 's3', 'cp', f'./{ancestry}/', s3_dir, '--recursive'])
+def upload_and_remove_files(ancestry, sub_region, regions):
+    for region in regions:
+        s3_dir = f'{s3_out}/out/ldsc/regions/ld_score/ancestry={ancestry}/{sub_region}/{region}/'
+        subprocess.check_call(['touch', f'./{ancestry}/{sub_region}/{region}/_SUCCESS'])
+        subprocess.check_call(['aws', 's3', 'cp', f'./{ancestry}/{sub_region}/{region}/', s3_dir, '--recursive'])
     shutil.rmtree(f'./{ancestry}')
 
 
-def run_ancestry(sub_region, ancestry):
-    get_all_annot_file(sub_region, ancestry)
-    combine_all_annot(ancestry)
-    make_ancestry_ld(ancestry)
-    upload_and_remove_files(sub_region, ancestry)
+def run_ancestry(ancestry, sub_region, regions):
+    get_all_annot_file(ancestry, sub_region, regions)
+    combine_all_annot(ancestry, sub_region, regions)
+    make_ancestry_ld(ancestry, sub_region, regions)
+    upload_and_remove_files(ancestry, sub_region, regions)
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--sub-region', default='default', type=str,
-                        help="Sub region name (default = default)")
     parser.add_argument('--ancestry', required=True, type=str,
                         help="Ancestry (g1000 style e.g. EUR)")
+    parser.add_argument('--sub-region', default='default', type=str,
+                        help="Sub region name (default = default)")
+    parser.add_argument('--regions', required=True, type=str,
+                        help="A list of regions in ancestry/sub_region to run")
     args = parser.parse_args()
-    sub_region = args.sub_region
     ancestry = args.ancestry
-    run_ancestry(sub_region, ancestry)
+    sub_region = args.sub_region
+    regions = args.regions.split(',')
+    run_ancestry(ancestry, sub_region, regions)
 
 
 if __name__ == '__main__':
