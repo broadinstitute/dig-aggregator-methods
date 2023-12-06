a8497c6af641009737da662eafbe1012743cb780
psmadbec <psmadbec@broadinstitute.org>
2023-12-06 13:42:07 -0500

    run genetic correlation against all portal phenotypes as well

--- ldsc/src/main/resources/downloadSumstatsFiles.py
+++ ldsc/src/main/resources/downloadSumstatsFiles.py
@@ -6,17 +6,19 @@ import subprocess
 downloaded_files = '/mnt/var/ldsc'
 sumstat_files = f'{downloaded_files}/sumstats'
 
+buckets = ['dig-analysis-data', 'dig-analysis-hermes']
 
-def download_ancestry_sumstats():
+def download_ancestry_sumstats(bucket):
     s3 = boto3.resource('s3')
-    my_bucket = s3.Bucket('dig-analysis-data')
+    my_bucket = s3.Bucket(bucket)
     for file in my_bucket.objects.filter(Prefix='out/ldsc/sumstats/').all():
         if re.fullmatch(f'.*\.sumstats\.gz$', file.key):
             ancestry = re.findall(f'.*_(\w+).sumstats.gz', file.key)[0]
             subprocess.check_call([
-                'sudo', 'aws', 's3', 'cp', f's3://dig-analysis-data/{file.key}', f'{sumstat_files}/{ancestry}/'
+                'sudo', 'aws', 's3', 'cp', f's3://{bucket}/{file.key}', f'{sumstat_files}/{ancestry}/'
             ])
 
 
 if __name__ == '__main__':
-    download_ancestry_sumstats()
+    for bucket in buckets:
+        download_ancestry_sumstats(bucket)
