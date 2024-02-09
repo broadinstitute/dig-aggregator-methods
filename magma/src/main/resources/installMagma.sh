a25fec3f6c35a8a8d8a6b43367cc5e0ed40c5e1b
psmadbec <psmadbec@broadinstitute.org>
2024-02-09 12:44:34 -0500

    [Magma] Fix magma for EMR 6.7

--- magma/src/main/resources/installMagma.sh
+++ magma/src/main/resources/installMagma.sh
@@ -4,26 +4,27 @@ S3DIR="s3://dig-analysis-data"
 MAGMA_DIR="/mnt/var/magma"
 
 # make the work directory
-mkdir -p "${MAGMA_DIR}"
+sudo mkdir -p "${MAGMA_DIR}"
 cd "${MAGMA_DIR}"
-mkdir -p "${MAGMA_DIR}"/pathway-genes
+sudo mkdir -p "${MAGMA_DIR}"/pathway-genes
 
 # copy data files
-aws s3 cp "${S3DIR}/bin/magma/NCBI37.3.gene.loc" .
+sudo aws s3 cp "${S3DIR}/bin/magma/NCBI37.3.gene.loc" .
 
-aws s3 cp "${S3DIR}/bin/magma/g1000_amr.zip" .
-unzip -o g1000_amr.zip
-aws s3 cp "${S3DIR}/bin/magma/g1000_afr.zip" .
-unzip -o g1000_afr.zip
-aws s3 cp "${S3DIR}/bin/magma/g1000_eas.zip" .
-unzip -o g1000_eas.zip
-aws s3 cp "${S3DIR}/bin/magma/g1000_eur.zip" .
-unzip -o g1000_eur.zip
-aws s3 cp "${S3DIR}/bin/magma/g1000_sas.zip" .
-unzip -o g1000_sas.zip
+sudo aws s3 cp "${S3DIR}/bin/magma/g1000_amr.zip" .
+sudo unzip -o g1000_amr.zip
+sudo aws s3 cp "${S3DIR}/bin/magma/g1000_afr.zip" .
+sudo unzip -o g1000_afr.zip
+sudo aws s3 cp "${S3DIR}/bin/magma/g1000_eas.zip" .
+sudo unzip -o g1000_eas.zip
+sudo aws s3 cp "${S3DIR}/bin/magma/g1000_eur.zip" .
+sudo unzip -o g1000_eur.zip
+sudo aws s3 cp "${S3DIR}/bin/magma/g1000_sas.zip" .
+sudo unzip -o g1000_sas.zip
 
-aws s3 cp "${S3DIR}/bin/magma/pathwayGenes.txt" "${MAGMA_DIR}"/pathway-genes/
+sudo aws s3 cp "${S3DIR}/bin/magma/pathwayGenes.txt" "${MAGMA_DIR}"/pathway-genes/
 
 # copy and extract the magma program
-aws s3 cp "${S3DIR}/bin/magma/magma_v1.07bb_static.zip" .
-unzip -o magma_v1.07bb_static.zip
+sudo aws s3 cp "${S3DIR}/bin/magma/magma_v1.07bb_static.zip" .
+sudo unzip -o magma_v1.07bb_static.zip
+sudo chmod 777 "${MAGMA_DIR}/magma"
