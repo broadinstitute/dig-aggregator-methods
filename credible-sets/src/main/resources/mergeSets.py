a94b89d459c28118245c97a279b7cb967840d4f6
psmadbec <psmadbec@broadinstitute.org>
2024-02-15 15:04:17 -0500

    Merge

--- credible-sets/src/main/resources/mergeSets.py
+++ credible-sets/src/main/resources/mergeSets.py
@@ -6,8 +6,8 @@ import os
 import shutil
 import subprocess
 
-s3_in = 's3://dig-giant-sandbox'
-s3_out = 's3://dig-giant-sandbox'
+s3_in = 's3://dig-analysis-data'
+s3_out = 's3://dig-analysis-data'
 
 
 def download_data(args):
