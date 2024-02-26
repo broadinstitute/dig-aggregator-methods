4d11dc8b0d4fcbf6d0d3c5e67c3219158f1325e3
psmadbec <psmadbec@broadinstitute.org>
2024-02-26 09:27:33 -0500

    missing comma

--- ldsc/src/main/scala/RegionToAnnotStage.scala
+++ ldsc/src/main/scala/RegionToAnnotStage.scala
@@ -15,7 +15,7 @@ class RegionToAnnotStage(implicit context: Context) extends Stage {
   override def cluster: ClusterDef = super.cluster.copy(
     instances = 1,
     applications = Seq.empty,
-    bootstrapScripts = Seq(new BootstrapScript(resourceUri("install-ldscore.sh")))
+    bootstrapScripts = Seq(new BootstrapScript(resourceUri("install-ldscore.sh"))),
     stepConcurrency = 7
   )
 
