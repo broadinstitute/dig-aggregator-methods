a4d29f7e71dd55a39951a65f0645a0b5e6da2506
psmadbec <psmadbec@broadinstitute.org>
2024-04-18 16:44:03 -0400

    a little reversion. Correlation fixes for private repos

--- ldsc/src/main/resources/runGeneticCorrelation.py
+++ ldsc/src/main/resources/runGeneticCorrelation.py
@@ -21,6 +21,7 @@ ldsc_files = f'{downloaded_files}/ldsc'
 sumstat_files = f'{downloaded_files}/sumstats'
 ldscore_files = f'{downloaded_files}/ldscore'
 
+project = os.environ['PROJECT']
 s3_in = os.environ['INPUT_PATH']
 s3_out = os.environ['OUTPUT_PATH']
 
@@ -35,18 +36,28 @@ def run_all(ancestry, phenotype, all_files):
     ])
 
 
-def upload_and_remove_output(ancestry, phenotype):
-    file = f'./{phenotype}_{ancestry}.log'
-    subprocess.check_call(['aws', 's3', 'cp', file, f'{s3_out}/out/ldsc/staging/genetic_correlation/ancestry={ancestry}/'])
-    os.remove(file)
+def upload_and_remove_output(ancestry, phenotype, other_project):
+    f_in = f'./h2.log'
+    f_out = f'{s3_out}/out/ldsc/staging/genetic_correlation/ancestry={ancestry}/{other_project}'
+    subprocess.check_call(['aws', 's3', 'cp', f_in, f'{f_out}/{phenotype}_{ancestry}.log'])
+    subprocess.check_call(['touch', '_SUCCESS'])
+    subprocess.check_call(['aws', 's3', 'cp', '_SUCCESS', f'{f_out}/_SUCCESS'])
+    os.remove(f_in)
+    os.remove('_SUCCESS')
+
+
+def run_project(ancestry, phenotype, main_file, other_project):
+    all_files = glob.glob(f'{sumstat_files}/{other_project}/*/ancestry={ancestry}/*.sumstats.gz')
+    all_files_but_main = [other_file for other_file in all_files if other_file != main_file]
+    run_all(ancestry, phenotype, f'{main_file},{",".join(all_files_but_main)}')
+    upload_and_remove_output(ancestry, phenotype, other_project)
 
 
 def run(ancestry, phenotype):
-    all_sumstats = glob.glob(f'{sumstat_files}/{ancestry}/*')
-    main_file = f'{sumstat_files}/{ancestry}/{phenotype}_{ancestry}.sumstats.gz'
-    other_files = [other_file for other_file in all_sumstats if other_file != main_file]
-    run_all(ancestry, phenotype, f'{main_file},{",".join(other_files)}')
-    upload_and_remove_output(ancestry, phenotype)
+    main_file = f'{sumstat_files}/{project}/{phenotype}/ancestry={ancestry}/{phenotype}_{ancestry}.sumstats.gz'
+    if os.path.exists(main_file):
+        for other_project in set(['portal', project]):
+            run_project(ancestry, phenotype, main_file, other_project)
 
 
 def main():
--- ldsc/src/main/scala/GeneticCorrelationStage.scala
+++ ldsc/src/main/scala/GeneticCorrelationStage.scala
@@ -1,6 +1,7 @@
 package org.broadinstitute.dig.aggregator.methods.ldsc
 
 import org.broadinstitute.dig.aggregator.core._
+import org.broadinstitute.dig.aws._
 import org.broadinstitute.dig.aws.emr._
 import org.broadinstitute.dig.aws.Ec2.Strategy
 import org.broadinstitute.dig.aws.MemorySize
@@ -8,14 +9,13 @@ import org.broadinstitute.dig.aws.MemorySize
 class GeneticCorrelationStage(implicit context: Context) extends Stage {
   import MemorySize.Implicits._
 
-  val sumstats: Input.Source = Input.Source.Success("out/ldsc/sumstats/*/*/")
+  val sumstats: Input.Source = Input.Source.Raw("out/ldsc/sumstats/*/*/*.sumstats.gz")
 
   /** Source inputs. */
   override val sources: Seq[Input.Source] = Seq(sumstats)
 
-  /** Map inputs to their outputs. */
   override val rules: PartialFunction[Input, Outputs] = {
-    case sumstats(_, ancestry) => Outputs.Named(ancestry.split('=').last)
+    case sumstats(_, ancestry, _) => Outputs.Named(ancestry.split('=').last)
   }
 
   /** Just need a single machine with no applications, but a good drive. */
@@ -25,16 +25,17 @@ class GeneticCorrelationStage(implicit context: Context) extends Stage {
     masterVolumeSizeInGB = 100,
     bootstrapScripts = Seq(
       new BootstrapScript(resourceUri("install-gc.sh")),
-      new BootstrapScript(resourceUri("downloadSumstatsFiles.py"))
+      new BootstrapScript(
+        resourceUri("downloadSumstatsFiles.py"), s"--input-path=s3://${context.s3.path}", s"--project=${context.project}"
+      )
     ),
-    stepConcurrency = 10,
-    masterInstanceType = Strategy.generalPurpose(mem = 64.gb)
+    stepConcurrency = 7
   )
 
   override def make(output: String): Job = {
     // Extract phenotype for only sumstats files for desired ancestry
     val pattern = s".*/([^/]+)/ancestry=$output/.*\\.sumstats\\.gz".r
-    val jobs = context.s3.ls(s"out/ldsc/sumstats/").flatMap { keyObject =>
+    val jobs: Seq[Job.Script] = context.s3.ls(s"out/ldsc/sumstats/").flatMap { keyObject =>
       keyObject.key match {
         case pattern(phenotype) =>
           Some(Job.Script(resourceUri("runGeneticCorrelation.py"), s"--phenotype=$phenotype", s"--ancestry=$output"))
@@ -49,12 +50,5 @@ class GeneticCorrelationStage(implicit context: Context) extends Stage {
    */
   override def prepareJob(output: String): Unit = {
     context.s3.rm(s"out/ldsc/staging/genetic_correlation/ancestry=$output/")
-  }
-
-  /** On success, write the _SUCCESS file in the output directory.
-   */
-  override def success(output: String): Unit = {
-    context.s3.touch(s"out/ldsc/staging/genetic_correlation/ancestry=$output/_SUCCESS")
-    ()
   }
 }
