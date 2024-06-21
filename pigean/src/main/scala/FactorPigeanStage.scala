2a7eee1a828d28bc2d9bca4f30a03b107a543c2b
psmadbec <psmadbec@broadinstitute.org>
2024-06-21 12:10:13 -0400

    Pigean factor

---
+++ pigean/src/main/scala/FactorPigeanStage.scala
@@ -0,0 +1,31 @@
+package org.broadinstitute.dig.aggregator.methods.pigean
+
+import org.broadinstitute.dig.aggregator.core._
+import org.broadinstitute.dig.aws._
+import org.broadinstitute.dig.aws.emr._
+
+class FactorPigeanStage(implicit context: Context) extends Stage {
+
+  override val cluster: ClusterDef = super.cluster.copy(
+    instances = 1,
+    bootstrapScripts = Seq(new BootstrapScript(resourceUri("pigean-bootstrap.sh")))
+  )
+
+  val pigean: Input.Source = Input.Source.Success("out/pigean/staging/pigean/*/")
+
+  override val sources: Seq[Input.Source] = Seq(pigean)
+
+  override val rules: PartialFunction[Input, Outputs] = {
+    case pigean(phenotype, sigmaPower, geneSetSize) => Outputs.Named(
+      s"$phenotype/${sigmaPower.split("sigma=").last}/${geneSetSize.split("size=").last}"
+    )
+  }
+
+  override def make(output: String): Job = {
+    val flags: Seq[String] = output.split("/").toSeq match {
+      case Seq(phenotype, sigmaPower, geneSetSize) =>
+        Seq(s"--phenotype=$phenotype", s"--sigma=$sigmaPower", s"--gene-set-size=$geneSetSize")
+    }
+    new Job(Job.Script(resourceUri("factorPigean.py"), flags:_*))
+  }
+}
