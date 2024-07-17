865e5be010340d95ceac68e2619c950a4ac186b4
psmadbec <psmadbec@broadinstitute.org>
2024-07-17 14:41:03 -0400

    Rename some columns

--- bioindex/src/main/resources/c2ct.py
+++ bioindex/src/main/resources/c2ct.py
@@ -11,6 +11,10 @@ outdir = f'{s3_bioindex}/credible_sets/c2ct/{{}}/{{}}'
 def build_bioindex(spark, key, bioindex_order):
     srcdir = f'{s3_in}/out/credible_sets/specificity/*/*/{key}.*.json'
     df = spark.read.json(srcdir)
+    df = df.withColumnRenamed('Q_all', 'Q') \
+        .withColumnRenamed('entPP_all', 'entPP') \
+        .withColumnRenamed('entropy_all', 'entropy') \
+        .withColumnRenamed('totalEntropy_all', 'totalEntropy')
     mixed_df = df[df['ancestry'] == 'Mixed']
     non_mixed_df = df[df['ancestry'] != 'Mixed']
     mixed_df.orderBy([col('phenotype')] + bioindex_order) \
@@ -26,11 +30,11 @@ def build_bioindex(spark, key, bioindex_order):
 def main():
     spark = SparkSession.builder.appName('bioindex').getOrCreate()
 
-    build_bioindex(spark, 'all', [col('Q_all').desc()])
-    build_bioindex(spark, 'annotation', [col('annotation'), col('Q_all').desc()])
-    build_bioindex(spark, 'tissue', [col('annotation'), col('tissue'), col('Q_all').desc()])
-    build_bioindex(spark, 'biosample', [col('annotation'), col('tissue'), col('biosample'), col('Q_all').desc()])
-    build_bioindex(spark, 'credible_set_id', [col('credibleSetId'), col('Q_all').desc()])
+    build_bioindex(spark, 'all', [col('Q').desc()])
+    build_bioindex(spark, 'annotation', [col('annotation'), col('Q').desc()])
+    build_bioindex(spark, 'tissue', [col('annotation'), col('tissue'), col('Q').desc()])
+    build_bioindex(spark, 'biosample', [col('annotation'), col('tissue'), col('biosample'), col('Q').desc()])
+    build_bioindex(spark, 'credible_set_id', [col('credibleSetId'), col('Q').desc()])
 
     # done
     spark.stop()
