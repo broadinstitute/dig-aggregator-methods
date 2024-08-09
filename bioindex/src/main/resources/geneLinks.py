ade84dd505c482f73fb0328560a8334048eaf57d
psmadbec <psmadbec@broadinstitute.org>
2024-08-09 14:46:15 -0400

    Update gene links tissue

--- bioindex/src/main/resources/geneLinks.py
+++ bioindex/src/main/resources/geneLinks.py
@@ -1,8 +1,11 @@
 import os
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import udf
 
 s3_in = os.environ['INPUT_PATH']
 s3_bioindex = os.environ['BIOINDEX_PATH']
+
+replace = udf(lambda s: s.replace('_', ' '))
 
 
 def main():
@@ -15,6 +18,7 @@ def main():
     outdir = f'{s3_bioindex}/regions/gene_links'
 
     df = spark.read.json(f'{srcdir}/part-*')
+    df = df.withColumn('tissue', replace('tissue'))
 
     # sort and write
     df.orderBy(['tissue', 'chromosome', 'start']) \
