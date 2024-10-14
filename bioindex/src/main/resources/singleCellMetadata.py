370fdb63681338cd469feaaf376754e378f54595
psmadbec <psmadbec@broadinstitute.org>
2024-10-14 12:43:22 -0400

    stash

--- bioindex/src/main/resources/singleCellMetadata.py
+++ bioindex/src/main/resources/singleCellMetadata.py
@@ -6,16 +6,16 @@ import json
 import shutil
 import subprocess
 
-s3_in = 's3://dig-analysis-data'
-s3_bioindex = 's3://dig-bio-index'
+s3_in = os.environ['INPUT_PATH']
+s3_bioindex = os.environ['BIOINDEX_PATH']
 
 
 def download_data():
     out = []
     cmd = 'aws s3 cp s3://dig-analysis-data/single_cell/ ./data/ ' \
-          '--recursive --exclude="*" --include="*dataset_metadata"'
+          '--recursive --exclude="*" --include="*dataset_metadata.json"'
     subprocess.check_call(cmd, shell=True)
-    for file in glob.glob('data/*/dataset_metadata'):
+    for file in glob.glob('data/*/dataset_metadata.json'):
         with open(file, 'r') as f:
             out.append(json.load(f))
     return out
