cb44c424bcf8439f6ce9e9de41c8f6a77e549888
psmadbec <psmadbec@broadinstitute.org>
2024-10-14 14:57:01 -0400

    Fix single cell metadata

--- bioindex/src/main/resources/singleCellMetadata.py
+++ bioindex/src/main/resources/singleCellMetadata.py
@@ -6,13 +6,13 @@ import json
 import shutil
 import subprocess
 
-s3_in = 's3://dig-analysis-data'
-s3_bioindex = 's3://dig-bio-index'
+s3_in = os.environ['INPUT_PATH']
+s3_bioindex = os.environ['BIOINDEX_PATH']
 
 
 def download_data():
     out = []
-    cmd = 'aws s3 cp s3://dig-analysis-data/single_cell/ ./data/ ' \
+    cmd = f'aws s3 cp {s3_in}}/single_cell/ ./data/ ' \
           '--recursive --exclude="*" --include="*dataset_metadata.json"'
     subprocess.check_call(cmd, shell=True)
     for file in glob.glob('data/*/dataset_metadata.json'):
