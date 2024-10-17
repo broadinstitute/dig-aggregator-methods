832a3887f51ee265cb0c2e876c016c7e8e0f0436
psmadbec <psmadbec@broadinstitute.org>
2024-10-17 15:01:06 -0400

    stash

--- bioindex/src/main/resources/kidsfirst-bootstrap.sh
+++ bioindex/src/main/resources/kidsfirst-bootstrap.sh
@@ -4,3 +4,4 @@ RAW_ROOT=/mnt/var/raw
 
 sudo aws s3 cp s3://dig-analysis-cfde/KidsFirst/bin/tissue_map.tsv ${RAW_ROOT}/
 sudo aws s3 cp s3://dig-analysis-cfde/KidsFirst/bin/phenotype_map.tsv ${RAW_ROOT}/
+sudo aws s3 cp s3://dig-analysis-cfde/catalog/out/pigean/gene_set_count/ ${RAW_ROOT}/gene_set_count/ --recursive
