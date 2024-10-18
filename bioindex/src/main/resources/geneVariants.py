0518ff6370c4daa4debef9b94bc77ac93bc1e3af
psmadbec <psmadbec@broadinstitute.org>
2024-10-18 11:10:33 -0400

    gene variants

--- bioindex/src/main/resources/geneVariants.py
+++ bioindex/src/main/resources/geneVariants.py
@@ -1,5 +1,6 @@
 import os
 from pyspark.sql import SparkSession, Row
+from pyspark.sql.functions import udf
 
 s3_in = os.environ['INPUT_PATH']
 s3_bioindex = os.environ['BIOINDEX_PATH']
@@ -16,16 +17,19 @@ def main():
     spark = SparkSession.builder.appName('bioindex').getOrCreate()
 
     # where to read input from
-    variants_dir = f'{s3_in}/variant_counts/*/*/*/part-*'
-    genes_dir = f's3://dig-analysis-bin/genes/GRCh37/part-*'
-    cqs_dir = f'{s3_in}/out/varianteffect/cqs/part-*'
-    common_dir = f'{s3_in}/out/varianteffect/common/part-*'
+    variants_dir = f'{s3_in}/variant_counts/*/*/*/*.json'
+    genes_dir = f's3://dig-analysis-cfde/KidsFirst/bin/genes/GRCh38/part-*'
+    # cqs_dir = f'{s3_in}/out/varianteffect/cqs/part-*'
+    # common_dir = f'{s3_in}/out/varianteffect/common/part-*'
+
+    apply_chr_fix = udf(lambda x: x.replace('chr', ''))
 
     # where to write the output to
     outdir = f'{s3_bioindex}/variants/gene'
 
     # load all the variant counts
     variants = spark.read.json(variants_dir)
+    variants = variants.withColumn('chromosome', apply_chr_fix(variants.chromosome))
 
     # load all genes, keep only canonical symbol entries
     genes = spark.read.json(genes_dir)
@@ -34,39 +38,39 @@ def main():
 
     # keep only variants overlapping genes
     overlap = (variants.chromosome == genes.chromosome) & \
-        (variants.position >= genes.start) & \
-        (variants.position < genes.end)
+              (variants.position >= genes.start) & \
+              (variants.position < genes.end)
 
     # inner join genes with overlapped variants
     df = genes.join(variants, on=overlap, how='inner') \
         .drop(genes.chromosome)
 
-    # join with common data per variant
-    common = spark.read.json(common_dir) \
-        .select('varId', 'dbSNP', 'consequence', 'nearest', 'minorAllele', 'maf', 'af')
-    df = df.join(common, on='varId', how='left')
-
-    # join with cqs data per variant (as list)
-    cqs = spark.read.json(cqs_dir)
-    cqs = cqs.drop('chromosome', 'position')
-
-    cols = [c for c in cqs.columns if c not in ['varId']]
-    flat_df = variants.join(cqs, on='varId', how='left')
-    vep_records = flat_df.rdd \
-        .keyBy(lambda r: r.varId) \
-        .combineByKey(
-            lambda row: [to_dict(row, cols)],
-            lambda rows, row: rows + [to_dict(row, cols)],
-            lambda rows1, rows2: rows1 + rows2
-        ) \
-        .map(lambda r: Row(
-            varId=r[0],
-            vepRecords=r[1]
-        )) \
-        .repartition(500) \
-        .toDF()
-
-    df = df.join(vep_records, on='varId', how='left')
+    # # join with common data per variant
+    # common = spark.read.json(common_dir) \
+    #     .select('varId', 'dbSNP', 'consequence', 'nearest', 'minorAllele', 'maf', 'af')
+    # df = df.join(common, on='varId', how='left')
+    #
+    # # join with cqs data per variant (as list)
+    # cqs = spark.read.json(cqs_dir)
+    # cqs = cqs.drop('chromosome', 'position')
+    #
+    # cols = [c for c in cqs.columns if c not in ['varId']]
+    # flat_df = variants.join(cqs, on='varId', how='left')
+    # vep_records = flat_df.rdd \
+    #     .keyBy(lambda r: r.varId) \
+    #     .combineByKey(
+    #         lambda row: [to_dict(row, cols)],
+    #         lambda rows, row: rows + [to_dict(row, cols)],
+    #         lambda rows1, rows2: rows1 + rows2
+    #     ) \
+    #     .map(lambda r: Row(
+    #         varId=r[0],
+    #         vepRecords=r[1]
+    #     )) \
+    #     .repartition(500) \
+    #     .toDF()
+    #
+    # df = df.join(vep_records, on='varId', how='left')
 
     # index by position
     df.orderBy(['gene']) \
