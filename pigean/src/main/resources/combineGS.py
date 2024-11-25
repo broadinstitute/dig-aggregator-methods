3f037726b59dc585281391ec394086b6a6ee4f7b
psmadbec <psmadbec@broadinstitute.org>
2024-11-25 14:06:30 -0500

    try split

--- pigean/src/main/resources/combineGS.py
+++ pigean/src/main/resources/combineGS.py
@@ -47,11 +47,14 @@ def convert_all_data():
 def combine(sigma, gene_set_size):
     if not os.path.exists('out'):
         os.mkdir('out')
-    with open(f'out/gs_{sigma}_{gene_set_size}.tsv', 'w') as f_out:
-        f_out.write('trait\tgene\tcombined\thuge\tlog_bf\n')
-        for file in glob.glob(f'data/*/*/sigma={sigma}/size={gene_set_size}/gs.tsv'):
-            with open(file, 'r') as f_in:
-                shutil.copyfileobj(f_in, f_out)
+    all_files = glob.glob(f'data/*/*/sigma={sigma}/size={gene_set_size}/gs.tsv')
+    groups = len(all_files) // 1000 + 1
+    for group in range(groups):
+        with open(f'out/gs_{sigma}_{gene_set_size}_{group}.tsv', 'w') as f_out:
+            f_out.write('trait\tgene\tcombined\thuge\tlog_bf\n')
+            for file in all_files[group*1000:(group+1)*1000]:
+                with open(file, 'r') as f_in:
+                    shutil.copyfileobj(f_in, f_out)
 
 
 def combine_all():
@@ -64,7 +67,7 @@ def combine_all():
 
 
 def upload_data():
-    subprocess.check_call(['aws', 's3', 'cp', 'out/', f'{s3_out}/out/pigean/staging/combined_gs/', '--recursive'])
+    subprocess.check_call(['aws', 's3', 'cp', 'out/', f'{s3_out}/out/pigean/staging/combined_gs/split/', '--recursive'])
     shutil.rmtree('out')
 
 
