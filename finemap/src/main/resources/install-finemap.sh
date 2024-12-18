a26de04be87d0b1b4276275a9533723214247941
szandavi <sm.zandavi@gmail.com>
2024-12-18 13:17:35 -0500

    udpate finemap

--- finemap/src/main/resources/install-finemap.sh
+++ finemap/src/main/resources/install-finemap.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -xe
 
+# susie method
+## Developed with python 3 and R
+
 finemap_ROOT=/mnt/var/cojo
 
 # install to the root directory
--- finemap/src/main/resources/makeFinemap.py
+++ finemap/src/main/resources/makeFinemap.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-from argparse import ArgumentParser
-from os import getenv
-from platform import python_version
-from pyspark.sql import SparkSession
-
 from optparse import OptionParser
 import pandas as pd
 import numpy as np
@@ -16,8 +11,8 @@ s3_out=os.environ['OUTPUT_PATH']
 
 # def finds json files in the directory
 def make_json_files(directory):
-    subprocess.check_call(['aws', 's3', 'cp', directory, 'input/', '--recursive'])
-    subprocess.run('zstdcat input/*.json.zst | jq -s '.' > input/input.json', shell=True)
+	subprocess.check_call(['aws', 's3', 'cp', directory, 'input/', '--recursive'])
+	subprocess.run('zstdcat input/*.json.zst | jq -s '.' > input/input.json', shell=True)
 
 def safe_remove(file_path):
     try:
@@ -31,42 +26,42 @@ def safe_remove(file_path):
         print(f"An error occurred while trying to remove {file_path}: {e}")
 
 def main():
-    usage = "usage: %prog [options]"
-    parser = OptionParser(usage)
-    parser.add_option("", "--phenotype", default=None)
-    parser.add_option("", "--ancestry", default=None)
+	usage = "usage: %prog [options]"
+	parser = OptionParser(usage)
+	parser.add_option("", "--phenotype", default=None)
+	parser.add_option("", "--ancestry", default=None)
 
-    (args,_) = parser.parse_args()
+	(args,_) = parser.parse_args()
 
-    pheno_path = f'{s3_in}/out/metaanalysis/bottom-line/ancestry-specific/{phenotype}/ancestry={ancestry}/'
-    var2rs_path = '/mnt/var/cojo/snps.csv'
-    bfiles = '/mnt/var/cojo/bfiles'
-    finemap_dir = '/mnt/var/cojo/finemapping'
-    config_file = '/mnt/var/cojo/finemapping/analysis.config.yaml'
-    out_path = f'{s3_out}/out/cojo/staging/{args.phenotype}/ancestry={args.ancestry}' 
+	pheno_path = f'{s3_in}/out/metaanalysis/bottom-line/ancestry-specific/{phenotype}/ancestry={ancestry}/'
+	var2rs_path = '/mnt/var/cojo/snps.csv'
+	bfiles = '/mnt/var/cojo/bfiles'
+	finemap_dir = '/mnt/var/cojo/finemapping'
+	config_file = '/mnt/var/cojo/finemapping/analysis.config.yaml'
+	out_path = f'{s3_out}/out/cojo/staging/{args.phenotype}/ancestry={args.ancestry}' 
 
-    # read all files in the clump path
-    make_json_files(pheno_path)
+	# read all files in the clump path
+	make_json_files(pheno_path)
 
-    # create the tmp out directory
-    out_directory = 'data'
-    if not os.path.exists(out_directory):
-        os.makedirs(out_directory, exist_ok=True)
+	# create the tmp out directory
+	out_directory = 'data'
+	if not os.path.exists(out_directory):
+		os.makedirs(out_directory, exist_ok=True)
 
-    subprocess.call(['bash', '/mnt/var/cojo/finemapping/run_finemap_pipeline.sh', 
-                    '--input','input'
-                    '--bfiles', bfiles,
-                    '--config_file',config_file,
-                    '--dbsnp_file',var2rs_path,
-                    '--output', out_directory,
-                    '--finemap_dir',finemap_dir
-                    ])
-    
-    subprocess.check_call(['touch', f'{out_directory}/_SUCCESS'])   
-    subprocess.check_call(['aws', 's3', 'cp', f'{out_directory}/', out_path, '--recursive'])
-    safe_remove('input/input.json')
-    shutil.rmtree('input')
-    shutil.rmtree(out_directory)
+	subprocess.call(['bash', '/mnt/var/cojo/finemapping/run_finemap_pipeline.sh', 
+					'--input','input'
+					'--bfiles', bfiles,
+					'--config_file',config_file,
+					'--dbsnp_file',var2rs_path,
+					'--output', out_directory,
+					'--finemap_dir',finemap_dir
+					])
+	
+	subprocess.check_call(['touch', f'{out_directory}/_SUCCESS'])	
+	subprocess.check_call(['aws', 's3', 'cp', f'{out_directory}/', out_path, '--recursive'])
+	safe_remove('input/input.json')
+	shutil.rmtree('input')
+	shutil.rmtree(out_directory)
 
 if __name__ == '__main__':
-    main()
+	main()
