a13595e4dcc61780fbe48feb2ba5010c05e8cc4f
szandavi <sm.zandavi@gmail.com>
2024-12-19 12:30:40 -0500

    update install-finemap.sh

--- finemap/src/main/resources/install-finemap.sh
+++ finemap/src/main/resources/install-finemap.sh
@@ -50,12 +50,12 @@ echo export PATH="$PWD:\$PATH" >> ~/.profile
 . ~/.profile
 
 # Install JRE
-sudo yum install -yf openjdk-8-jre-headless openjdk-8-jdk
+sudo yum install -y openjdk-8-jre-headless openjdk-8-jdk
 # sudo update-java-alternatives --list
 # sudo update-java-alternatives --set java-1.8.0-openjdk-amd64
 
 # Install parallel
-sudo yum install -yf parallel
+sudo yum install -y parallel
 
 echo COMPLETE
 
