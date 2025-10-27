4ac016ab707d58c0f0cf3986a5e269c380d7dbfe
psmadbec <psmadbec@broadinstitute.org>
2025-10-27 15:22:53 -0400

    Chase change mark 1

--- pigean/src/main/resources/pigean-bootstrap.sh
+++ pigean/src/main/resources/pigean-bootstrap.sh
@@ -13,6 +13,7 @@ sudo aws s3 cp s3://dig-analysis-data/out/pigean/gene_lists/ . --recursive
 sudo aws s3 cp s3://dig-analysis-bin/pigean/gene_sets/ . --recursive
 sudo aws s3 cp s3://dig-analysis-bin/pigean/methods/ . --recursive
 sudo aws s3 cp s3://dig-analysis-bin/pigean/misc/ . --recursive
+sudo aws s3 cp s3://dig-analysis-bin/pigean/models/ . --recursive
 
 sudo aws s3 cp s3://dig-analysis-bin/orphanet/code_to_leaves.tsv .
 
