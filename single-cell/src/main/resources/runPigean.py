c250e33d265ba7ac5e4821270635e8f28fa828a9
psmadbec <psmadbec@broadinstitute.org>
2026-05-07 12:08:56 -0400

    stash

--- single-cell/src/main/resources/runPigean.py
+++ single-cell/src/main/resources/runPigean.py
@@ -5,7 +5,6 @@ import json
 import os
 import shutil
 import subprocess
-import zipfile
 
 
 downloaded_files = '/mnt/var/pigean'
@@ -59,112 +58,59 @@ def make_positive_controls(idx):
                 f_out.write(f'{split_line[0]}\t{prob}\n')
 
 
-def run_all(model, openapi_key):
-    with open('input/factor_matrix_gene_probs.tsv', 'r') as f:
-        num_cols = len(f.readline().strip().split('\t'))
-    for idx in range(1, num_cols):
-        make_positive_controls(idx)
-        output = f'factor_{idx-1}'
-        subprocess.run(['python', f'{downloaded_files}/priors-251215-mod.py', 'naive_factor',
-                       '--gene-map-in', f'{downloaded_files}/portal_gencode.gene.map',
-                       '--max-num-gene-sets', '5000',
-                       '--gene-filter-value', '1',
-                       '--gene-set-filter-value', '0.01',
-                       '--positive-controls-in', 'positive_controls_in.txt',
-                       '--positive-controls-id-col', 'gene',
-                       '--positive-controls-prob-col', 'prob',
-                       '--positive-controls-all-in', f'{downloaded_files}/NCBI37.3.plink.gene.loc',
-                       '--positive-controls-all-id-col', '6',
-                       '--positive-controls-all-no-header',
-                       '--gene-stats-out', f'staging/gs.{output}.out',
-                       '--gene-set-stats-out', f'staging/gss.{output}.out',
-                       '--gene-gene-set-stats-out', f'staging/ggss.{output}.out',
-                       '--gene-effectors-out', f'staging/ge.{output}.out',
-                       '--factors-out', f'staging/f.{output}.out',
-                       '--gene-clusters-out', f'staging/gc.{output}.out',
-                       '--pheno-clusters-out', f'staging/pc.{output}.out',
-                       '--gene-set-clusters-out', f'staging/gsc.{output}.out',
-                       '--params-out', f'staging/p.{output}.out',
-                       '--factor-phewas-stats-out', f'staging/fphs.{output}.out',
-                       '--phewas-stats-out', f'staging/phs.{output}.out',
-                       '--gene-set-phewas-stats-in', f'{downloaded_files}/gss_{model_to_gene_stats[model]}.tsv',
-                       '--gene-set-phewas-stats-id-col', 'gene_set',
-                       '--gene-set-phewas-stats-pheno-col', 'trait',
-                       '--gene-set-phewas-stats-beta-uncorrected-col', 'beta_uncorrected',
-                       '--gene-phewas-stats-in', f'{downloaded_files}/gs_{model_to_gene_stats[model]}.tsv',
-                       '--gene-phewas-bfs-id-col', 'gene',
-                       '--gene-phewas-bfs-pheno-col', 'trait',
-                       '--gene-phewas-bfs-combined-col', 'combined',
-                       '--gene-phewas-bfs-log-bf-col', 'log_bf',
-                       '--run-phewas-from-gene-phewas-stats-in', f'{downloaded_files}/gs_{model_to_gene_stats[model]}.tsv',
-                       '--factor-phewas-from-gene-phewas-stats-in', f'{downloaded_files}/gs_{model_to_gene_stats[model]}.tsv']
-                       + (['--lmm-auth-key', f'{openapi_key}'] if openapi_key is not None else [])
-                       + get_gene_sets(model_to_gene_stats[model]))
-    os.remove('positive_controls_in.txt')
-    return num_cols
-
 def run(model):
     with open('input/factor_matrix_gene_probs.tsv', 'r') as f:
-        num_cols = len(f.readline().strip().split('\t'))
-    for idx in range(1, num_cols):
+        header = f.readline().strip().split('\t')
+    for idx in range(1, len(header)):
         make_positive_controls(idx)
-        output = f'factor_{idx-1}'
-        subprocess.run(['python', f'{downloaded_files}/priors.py', 'naive_priors',
+        print(' '.join(['python3.11', '-m', 'pigean', 'gibbs',
                         '--gene-map-in', f'{downloaded_files}/portal_gencode.gene.map',
                         '--max-num-gene-sets', '5000',
-                        '--gene-filter-value', '1',
-                        '--gene-set-filter-value', '0.01',
-                        '--positive-controls-in', 'positive_controls_in.txt',
+                        '--positive-controls-in', os.path.abspath('positive_controls_in.txt'),
                         '--positive-controls-id-col', 'gene',
                         '--positive-controls-prob-col', 'prob',
                         '--positive-controls-all-in', f'{downloaded_files}/NCBI37.3.plink.gene.loc',
                         '--positive-controls-all-id-col', '6',
                         '--positive-controls-all-no-header',
-                        '--gene-stats-out', f'staging/gs.{output}.out',
-                        '--gene-set-stats-out', f'staging/gss.{output}.out',
-                        '--gene-gene-set-stats-out', f'staging/ggss.{output}.out',
-                        '--gene-effectors-out', f'staging/ge.{output}.out']
-                       + get_gene_sets(model_to_gene_stats[model]))
+                        '--gene-set-stats-out', os.path.abspath(f'staging/gss.{header[idx]}.out')]
+                       + get_gene_sets(model_to_gene_stats[model])))
+        subprocess.run(['python3.11', '-m', 'pigean', 'gibbs',
+                        '--gene-map-in', f'{downloaded_files}/portal_gencode.gene.map',
+                        '--max-num-gene-sets', '5000',
+                        '--positive-controls-in', os.path.abspath('positive_controls_in.txt'),
+                        '--positive-controls-id-col', 'gene',
+                        '--positive-controls-prob-col', 'prob',
+                        '--positive-controls-all-in', f'{downloaded_files}/NCBI37.3.plink.gene.loc',
+                        '--positive-controls-all-id-col', '6',
+                        '--positive-controls-all-no-header',
+                        '--gene-set-stats-out', os.path.abspath(f'staging/gss.{header[idx]}.out')]
+                       + get_gene_sets(model_to_gene_stats[model]),
+                       cwd=f'{downloaded_files}/pigean/src')
     os.remove('positive_controls_in.txt')
     return
-
-
-def combine_top_gene_sets():
-    with open('input/factor_matrix_gene_probs.tsv', 'r') as f:
-        num_cols = len(f.readline().strip().split('\t'))
-
-    with open('output/pigean.top_gene_sets.tsv', 'w') as f_out:
-        f_out.write('factor\tgene_sets\n')
-        for idx in range(1, num_cols):
-            with open(f'staging/gss.factor_{idx-1}.out', 'r') as f:
-                _ = f.readline()
-                f_out.write('{}\t{}\n'.format(idx-1, ','.join([f.readline().strip().split('\t')[0] for idx in range(5)])))
-
 
 def combine_gene_sets():
     with open('input/factor_matrix_gene_probs.tsv', 'r') as f:
         num_cols = len(f.readline().strip().split('\t'))
 
     with open('output/pigean.gene_sets.tsv', 'w') as f_out:
-        f_out.write('factor\tgene_set\tbeta\n')
+        f_out.write('factor\tgene_set\tbeta\tbeta_uncorrected\n')
         for idx in range(1, num_cols):
-            with open(f'staging/gss.factor_{idx-1}.out', 'r') as f:
+            with open(f'staging/gss.Factor{idx}.out', 'r') as f:
                 header = f.readline().strip().split('\t')
                 for line in f:
                     line_dict = dict(zip(header, line.strip().split('\t')))
-                    f_out.write('{}\t{}\t{}\n'.format(
-                        idx - 1,
+                    f_out.write('{}\t{}\t{}\t{}\n'.format(
+                        f'Factor{idx}',
                         line_dict['Gene_Set'],
-                        line_dict['beta']
+                        line_dict['beta'],
+                        line_dict['beta_uncorrected']
                     ))
 
 
 def upload(dataset, cell_type, model):
     staging_output = f'{s3_out}/out/single_cell/staging/pigean/{dataset}/{cell_type}/{model}'
-    with zipfile.ZipFile('pigean.results.zip', 'w', zipfile.ZIP_DEFLATED) as z:
-        for file in glob.glob('staging/*.out'):
-            z.write(file)
-    subprocess.check_call(['aws', 's3', 'cp', 'pigean.results.zip', f'{staging_output}/'])
+    subprocess.check_call(['aws', 's3', 'cp', 'staging/', f'{staging_output}/', '--recursive'])
 
     output = f'{s3_out}/out/single_cell/pigean/{dataset}/{cell_type}/{model}'
     subprocess.check_call(['aws', 's3', 'cp', 'output/', f'{output}/', '--recursive'])
@@ -182,9 +128,8 @@ def main():
     os.makedirs('staging', exist_ok=True)
     run(args.model)
 
-    if len(glob.glob('staging/gs.*')) > 0:
+    if len(glob.glob('staging/gss.*')) > 0:
         os.makedirs('output', exist_ok=True)
-        combine_top_gene_sets()
         combine_gene_sets()
         upload(args.dataset, args.cell_type, args.model)
         shutil.rmtree('output')
