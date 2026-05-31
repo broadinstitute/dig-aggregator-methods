2ecbeeb1bd36119874305239750af3041b913abf
psmadbec <psmadbec@broadinstitute.org>
2026-05-30 21:40:38 -0400

    worth a shot

--- single-cell/src/main/resources/makeH5ad.py
+++ single-cell/src/main/resources/makeH5ad.py
@@ -2,6 +2,7 @@
 import argparse
 import anndata as ad
 import gzip
+import math
 import os
 from scipy.sparse import csc_matrix, vstack
 import shutil
@@ -19,27 +20,30 @@ def download_data(dataset):
 def get_metadata_maps():
     cell_type_map = {}
     donor_map = {}
+    ncount_map = {}
     with gzip.open('inputs/sample_metadata.tsv.gz', 'rt') as f:
         header = f.readline().strip().split('\t')
         for line in f:
             json_line = dict(zip(header, line.strip().split('\t')))
-            n_count = json_line['nCount_RNA']
+            n_count = json_line['QC:nCount_RNA']
             if len(n_count) > 0 and float(n_count) % 1 == 0:
                 cell_type_map[json_line['ID']] = json_line['cell_type__kp']
                 donor_map[json_line['ID']] = json_line['Dataset']
-    return cell_type_map, donor_map
+                ncount_map[json_line['ID']] = n_count
+    return cell_type_map, donor_map, ncount_map
 
 
-def get_sparse_array(cell_type_map, donor_map):
+def get_sparse_array(cell_type_map, donor_map, ncount_map):
     cell_types = list(set(cell_type_map.values()))
     with gzip.open('inputs/norm_counts.tsv.gz', 'rt') as f:
         cells = f.readline().strip().split('\t')[1:]
         cells_idx_dict = {cell_type: [idx for idx, cell in enumerate(cells) if cell in cell_type_map and cell_type_map[cell] == cell_type] for cell_type in cell_types}
+        ncount_idx_dict = {cell_type: [ncount_map[cell] for cell in cells if cell in cell_type_map and cell_type_map[cell] == cell_type] for cell_type in cell_types}
         gene, data = f.readline().strip().split('\t', 1)
         genes = [gene]
         formatted_data = list(map(float, data.split('\t')))
         # A = csc_matrix([formatted_data])
-        A_dict = {cell_type: csc_matrix([[formatted_data[idx] for idx in cells_idx_dict[cell_type]]]) for cell_type in cell_types}
+        A_dict = {cell_type: csc_matrix([[int(round((math.exp(formatted_data[idx]) - 1) * ncount_idx_dict[cell_type][idx] / 1E4)) for idx in cells_idx_dict[cell_type]]]) for cell_type in cell_types}
         count = 1
         idx = 0
         # B = []
@@ -59,7 +63,7 @@ def get_sparse_array(cell_type_map, donor_map):
                 line_to_append = list(map(float, data.split('\t')))
                 # B.append(line_to_append)
                 for cell_type in cell_types:
-                    B_dict[cell_type].append([line_to_append[idx] for idx in cells_idx_dict[cell_type]])
+                    B_dict[cell_type].append([int(round((math.exp(line_to_append[idx]) - 1) * ncount_idx_dict[cell_type][idx] / 1E4)) for idx in cells_idx_dict[cell_type]])
                 count += 1
                 genes.append(gene)
         # A = vstack([A, csc_matrix(B)])
@@ -110,8 +114,8 @@ def run():
     args = parser.parse_args()
 
     download_data(args.dataset)
-    cell_type_map, donor_map = get_metadata_maps()
-    cell_type_adata_dict = get_sparse_array(cell_type_map, donor_map)
+    cell_type_map, donor_map, ncount_map = get_metadata_maps()
+    cell_type_adata_dict = get_sparse_array(cell_type_map, donor_map, ncount_map)
     upload(args.dataset, cell_type_adata_dict)
 
 
