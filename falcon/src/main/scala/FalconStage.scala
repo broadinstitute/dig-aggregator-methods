c4d7c9ac4549be514130e1a6eb9d7048794d39da
psmadbec <psmadbec@broadinstitute.org>
2026-08-17 15:01:21 -0400

    not good enough to warrant double cost

--- falcon/src/main/scala/FalconStage.scala
+++ falcon/src/main/scala/FalconStage.scala
@@ -23,7 +23,7 @@ class FalconStage(implicit context: Context) extends Stage {
     instances = 1,
     applications = Seq.empty,
     masterVolumeSizeInGB = 100,
-    masterInstanceType = Strategy.computeOptimized(vCPUs = 16, mem = 32.gb),
+    masterInstanceType = Strategy.computeOptimized(),
     bootstrapScripts = Seq(new BootstrapScript(resourceUri("bootstrap_falcon.sh")))
   )
 
