dfe59f6ba24d2a478e10c820f9a5a4984bad236e
psmadbec <psmadbec@broadinstitute.org>
2026-08-21 09:29:39 -0400

    stash

--- falcon/src/main/resources/falcon.py
+++ falcon/src/main/resources/falcon.py
@@ -45,7 +45,7 @@ def reformat_sumstats(snp_map):
             rsid = snp_map.get(row['varId'])
             if rsid is None:
                 continue
-            if abs(row['beta'] / row['stdErr']) <= 5:
+            if row['stdErr'] == 0 or abs(row['beta'] / row['stdErr']) <= 5:
                 continue
             if chrom not in out_files:
                 f = open(f'inputs/sumstats/{chrom}.sumstats', 'w')
