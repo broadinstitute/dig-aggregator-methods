dcd650aedee2668de0c2f10960fb1c97253eee36
psmadbec <psmadbec@broadinstitute.org>
2026-09-14 10:55:27 -0400

    stash

--- single-cell/src/main/scala/MakeH5adStage.scala
+++ single-cell/src/main/scala/MakeH5adStage.scala
@@ -11,6 +11,7 @@ class MakeH5adStage(implicit context: Context) extends Stage {
   override val cluster: ClusterDef = super.cluster.copy(
     instances = 1,
     masterInstanceType = Strategy.memoryOptimized(mem = 256.gb),
+    masterVolumeSizeInGB = 100,
     bootstrapScripts = Seq(new BootstrapScript(resourceUri("bootstrap-h5ad.sh")))
   )
 
